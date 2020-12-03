a77dc346e553efa6e8e3d81e8e23a6116a8a642e
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-03 10:09:29 +0100

    Initial commit

---
+++ 2020.fsx
@@ -0,0 +1,17 @@
+open System.IO
+
+let readInput day =
+    let inputFilePath = Path.Combine("2020", $"{day}.txt")
+    File.ReadAllLines(inputFilePath)
+
+let readInputAsIntegers day = readInput day |> Array.map int
+
+let solveDay1 () =
+    let numbers = readInputAsIntegers 1
+
+    [ for n1 in numbers do
+        for n2 in numbers do
+            if n1 + n2 = 2020 then yield n1 * n2 ]
+    |> List.head
+
+printfn $"Day 1: {solveDay1 ()}"
