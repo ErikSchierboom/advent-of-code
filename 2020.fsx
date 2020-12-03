07d1616620dd872e245cb6018bbe869b51a0be40
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-03 13:36:13 +0100

    [2020/1] Solve

--- 2020.fsx
+++ 2020.fsx
@@ -11,9 +11,20 @@ let readInputAsIntegers day = readInput day |> Array.map int
 let solveDay1 () =
     let numbers = readInputAsIntegers 1
 
-    [ for n1 in numbers do
-        for n2 in numbers do
-            if n1 + n2 = 2020 then yield n1 * n2 ]
-    |> List.head
+    let part1 =
+        [ for n1 in numbers do
+            for n2 in numbers do
+                if n1 + n2 = 2020 then yield n1 * n2 ]
+        |> List.head
 
-printfn "Day 1: %i" (solveDay1 ())
+    let part2 =
+        [ for n1 in numbers do
+            for n2 in numbers do
+                for n3 in numbers do
+                    if n1 + n2 + n3 = 2020 then yield n1 * n2 * n3 ]
+        |> List.head
+
+    (part1, part2)
+
+
+printfn "Day 1: %A" (solveDay1 ())
