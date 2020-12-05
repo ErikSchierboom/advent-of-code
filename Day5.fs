251910aac0a12c3168a3e154e487450cbaab349c
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-05 16:42:29 +0100

    Small refactoring

--- Day5.fs
+++ Day5.fs
@@ -4,7 +4,7 @@ let boardingPasses = Input.asLines 5
 
 let seatId (boardingPass: string) =
     boardingPass
-    |> Seq.fold (fun state encoding -> if encoding = 'B' || encoding = 'R' then state <<< 1 ||| 1 else state <<< 1) 0
+    |> Seq.fold (fun acc elem -> if elem = 'B' || elem = 'R' then acc <<< 1 ||| 1 else acc <<< 1) 0
 
 let seatIds = boardingPasses |> Seq.map seatId
 
