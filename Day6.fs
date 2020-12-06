30d2b9cd88aa4e738c85c1122d9e68b423ae09db
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-06 11:51:58 +0100

    Solve day 6

---
+++ Day6.fs
@@ -0,0 +1,15 @@
+﻿module AdventOfCode.Day6
+
+module String =
+    let split (separator: string) (str: string) = str.Split(separator) 
+
+let groupAnswers combiner =
+    Input.asString 6
+    |> String.split "\n\n"
+    |> Seq.map (String.split "\n" >> Seq.map set)
+    |> Seq.sumBy (Seq.reduce combiner >> Set.count)
+
+let part1 = groupAnswers Set.union
+let part2 = groupAnswers Set.intersect
+
+let solution = part1, part2
--- Program.fs
+++ Program.fs
@@ -7,5 +7,6 @@ let main _ =
     printfn "Day 3: %A" Day3.solution
     printfn "Day 4: %A" Day4.solution
     printfn "Day 5: %A" Day5.solution
+    printfn "Day 6: %A" Day6.solution
 
     0
