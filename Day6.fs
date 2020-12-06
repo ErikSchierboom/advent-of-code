78a362123fd52e2e4fdc95bece6c3c067cafa3d4
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-06 11:52:50 +0100

    Minor nameing refactoring

--- Day6.fs
+++ Day6.fs
@@ -3,11 +3,11 @@
 module String =
     let split (separator: string) (str: string) = str.Split(separator) 
 
-let groupAnswers combiner =
+let groupAnswers reducer =
     Input.asString 6
     |> String.split "\n\n"
     |> Seq.map (String.split "\n" >> Seq.map set)
-    |> Seq.sumBy (Seq.reduce combiner >> Set.count)
+    |> Seq.sumBy (Seq.reduce reducer >> Set.count)
 
 let part1 = groupAnswers Set.union
 let part2 = groupAnswers Set.intersect
