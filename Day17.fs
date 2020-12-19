cfaae3b7193e9627853c9e758ec0d37b26bd0625
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-19 12:36:56 +0100

    Working

--- Day17.fs
+++ Day17.fs
@@ -2,11 +2,14 @@
 
 // TODO: use array; calculate from dimensions * number of round
 
+let input = Input.asLines 17
+let dimension = input.Length
+
 let initialState =
-    Input.asLines 17
+    input
     |> Seq.indexed
-    |> Seq.collect (fun (y, line) -> line |> Seq.indexed |> Seq.map (fun (x, letter) -> (x, y, 0, 0), letter = '#' ))
-    |> Map.ofSeq
+    |> Seq.collect (fun (y, line) -> line |> Seq.indexed |> Seq.choose (fun (x, letter) -> if letter = '#' then Some (x, y, 0, 0) else None))
+    |> Set.ofSeq
 
 let neighbors3d (x, y, z, w) =
     [for dx in -1..1 do
@@ -14,6 +17,14 @@ let neighbors3d (x, y, z, w) =
      for dz in -1..1 do
         if dx <> 0 || dy <> 0 || dz <> 0 then
             yield x + dx, y + dy, z + dz, 0]
+    
+let coordinatesForCycle cycle =
+    [for dx in -1..1 do
+     for dy in -1..1 do
+     for dz in -1..1 do
+     for dw in -1..1 do
+        if dx <> 0 || dy <> 0 || dz <> 0 || dw <> 0 then
+            yield x + dx, y + dy, z + dz, w + dw]
     
 let neighbors4d (x, y, z, w) =
     [for dx in -1..1 do
@@ -23,7 +34,7 @@ let neighbors4d (x, y, z, w) =
         if dx <> 0 || dy <> 0 || dz <> 0 || dw <> 0 then
             yield x + dx, y + dy, z + dz, w + dw]
 
-let isActive state coord = Map.tryFind coord state |> Option.map ((=) true) |> Option.defaultValue false
+let isActive state coord = Set.contains coord state
 
 let activeNeighbors dimensionNeighbors state coord =
     dimensionNeighbors coord
@@ -34,37 +45,39 @@ let applyRulesToCoord dimensionNeighbors originalState newState coord =
     match isActive originalState coord, activeNeighbors dimensionNeighbors originalState coord with
     | true,  2
     | true,  3
-    | false, 3 -> Map.add coord true newState
-    | _, _     -> Map.add coord false newState
+    | false, 3 -> Set.add coord newState
+    | _, _     -> Set.remove coord newState
 
-
-let applyRules dimensionNeighbors state =
-    state
+let activeCubesAfterCycles dimensionNeighbors state =
+    let cycles = 6
+    
+    let rec loop currentCycle currentState =
+        if currentCycle = cycles then
+            Set.count currentState
+        else
+            [for dx in -1..1 do
+             for dy in -1..1 do
+             for dz in -1..1 do
+             for dw in -1..1 do
+                if dx <> 0 || dy <> 0 || dz <> 0 || dw <> 0 then
+                    yield x + dx, y + dy, z + dz, w + dw]
+            state
     |> Map.toSeq
     |> Seq.map fst
     |> Seq.collect dimensionNeighbors
     |> set
     |> Seq.fold (applyRulesToCoord dimensionNeighbors state) state
-
-let part1 =
-    let x =
-        initialState
-        |> applyRules neighbors3d
-        |> applyRules neighbors3d
-        |> applyRules neighbors3d
-        |> applyRules neighbors3d
-        |> applyRules neighbors3d
-        |> applyRules neighbors3d
+            
+            
+    loop 0 state
         
-    let y =
-        x
-        |> Map.toSeq
-        |> Seq.map snd
-        |> Seq.filter ((=) true)
-        |> Seq.length
     
-    y
     
-let part2 = 0
+
+let part1 = activeCubesAfterCycles neighbors3d
+    
+let part2 =
+//    activeCubesAfterCycles neighbors34
+    0
 
 let solution = part1, part2
