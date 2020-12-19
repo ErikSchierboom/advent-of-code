2f9a367a8a1e6c1bb536488b232bda76c3e0a033
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-19 18:42:12 +0100

    Day17 (#1)
    * Solve day 17
    
    * Working
    
    * Working
    
    * Solve day 17
    
    * Naive implementation
    
    * Use struct tuples
    
    * Use struct tuples
    
    * Revert

---
+++ Day17.fs
@@ -0,0 +1,58 @@
+﻿module AdventOfCode.Day17
+
+let input = Input.asLines 17
+let dimension = input.Length
+
+let initialState =
+    input
+    |> Seq.indexed
+    |> Seq.collect (fun (y, line) -> line |> Seq.indexed |> Seq.choose (fun (x, letter) -> if letter = '#' then Some (x, y, 0, 0) else None))
+    |> Set.ofSeq
+
+let neighbors3d (x, y, z, w) =
+    seq {
+        for dx in -1..1 do
+        for dy in -1..1 do
+        for dz in -1..1 do
+           if dx <> 0 || dy <> 0 || dz <> 0 then
+               yield (x + dx, y + dy, z + dz, 0)   
+    } |> Set.ofSeq
+    
+let neighbors4d (x, y, z, w) =
+    seq {
+        for dx in -1..1 do
+        for dy in -1..1 do
+        for dz in -1..1 do
+        for dw in -1..1 do
+            if dx <> 0 || dy <> 0 || dz <> 0 || dw <> 0 then
+                yield (x + dx, y + dy, z + dz, w + dw)
+    } |> Set.ofSeq
+
+let isActive state coord = Set.contains coord state
+
+let activeNeighbors dimensionNeighbors state coord = Set.intersect state (dimensionNeighbors coord) |> Set.count
+
+let applyRulesToCoord dimensionNeighbors originalState newState coord =
+    match isActive originalState coord, activeNeighbors dimensionNeighbors originalState coord with
+    | true,  2
+    | true,  3
+    | false, 3 -> Set.add coord newState
+    | _, _     -> newState
+
+let activeCubesAfterCycles dimensionNeighbors =
+    let rec loop currentCycle currentState =
+        if currentCycle = 6 then
+            Set.count currentState
+        else
+            currentState
+            |> Seq.collect dimensionNeighbors
+            |> Set.ofSeq
+            |> Seq.fold (applyRulesToCoord dimensionNeighbors currentState) Set.empty
+            |> loop (currentCycle + 1)
+
+    loop 0 initialState
+
+let part1 = activeCubesAfterCycles neighbors3d
+let part2 = activeCubesAfterCycles neighbors4d
+
+let solution = part1, part2
