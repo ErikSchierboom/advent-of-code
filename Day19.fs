2970469c9ed4e249aee9913e346328cb01e5873d
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-20 19:20:15 +0100

    Parse all

--- Day19.fs
+++ Day19.fs
@@ -34,35 +34,19 @@ let pConstant = pNumber .>> pSeparator .>>. (between (pchar '"') (pchar '"') pLe
 let pSequence = pNumber .>> pSeparator .>>. pNumbers |>> Sequence
 let pOneOrOther = pNumber .>> pSeparator .>>. pNumbers .>> spaces .>> pstring "|" .>> spaces .>>. pNumbers |>> fun ((number, left), right) -> OneOrOther (number, left, right)
 let pRule = choice [attempt pConstant; attempt pOneOrOther; pSequence]
-
 let pMessage = many1Chars pLetter
-
+let pInput = sepEndBy1 pRule newline .>> skipNewline .>>. sepEndBy1 pMessage newline
 
 let runParser parser input =
     match run parser input with
     | Success (result, _, _) -> result
     | Failure (error, _, _) -> failwith error
 
-//0: 4 1 5
-//1: 2 3 | 3 2
-//2: 4 4 | 5 5
-//3: 4 5 | 5 4
-//4: "a"
-//
-
-printfn "%A" (run pConstant "5: \"b\"")
-printfn "%A" (run pSequence "0: 4 1 5")
-printfn "%A" (run pOneOrOther "3: 4 5 | 5 4")
-printfn "%A" (run pMessage "abasd")
-
-//let rules, messages =
-//    let lines = Input.asLines 19
-//    let rules = lines |> Seq.takeWhile (fun line -> line.Length <> 0) |> Seq.map parseRule
-//    let messages = lines |> Seq.skipWhile (fun line -> line.Length <> 0) |> Seq.tail |> Seq.toArray    
-//    rules, messages
+let rules, messages = runParser pInput (Input.asString 19)
 
 let part1 =
-//    printfn "%A" (rules |> Seq.toArray)
+    printfn "%A" rules
+    printfn "%A" messages
     0
 //    messages
 //    |> Seq.filter matches
@@ -72,3 +56,31 @@ let part1 =
 let part2 = 0
 
 let solution = part1, part2
+
+
+//0: 4 1 5
+//1: 2 3 | 3 2
+//2: 4 4 | 5 5
+//3: 4 5 | 5 4
+//4: "a"
+//
+
+//let input =
+//    "0: 4 1 5\n" +
+//    "1: 2 3 | 3 2\n" +
+//    "2: 4 4 | 5 5\n" +
+//    "3: 4 5 | 5 4\n" +
+//    "4: \"a\"\n" +
+//    "5: \"b\"\n" +
+//    "\n" +
+//    "ababbb\n" +
+//    "bababa\n" +
+//    "abbbab\n" +
+//    "aaabbb\n" +
+//    "aaaabbb"
+//
+//printfn "%A" (run pConstant "5: \"b\"")
+//printfn "%A" (run pSequence "0: 4 1 5")
+//printfn "%A" (run pOneOrOther "3: 4 5 | 5 4")
+//printfn "%A" (run pMessage "abasd")
+//printfn "%A" (run pInput input)
