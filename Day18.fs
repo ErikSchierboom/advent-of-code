bad7c911b6a5cbf84bcaa6e4392768adee924037
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-20 11:40:53 +0100

    Solve part 2

--- Day18.fs
+++ Day18.fs
@@ -4,21 +4,27 @@ open FParsec
 
 let equations = Input.asLines 18
 
-let pTerm = puint64 .>> spaces
-let pChar c = pchar c .>> spaces
-let opp = OperatorPrecedenceParser<_, _, _>()
-let pExpression = opp.ExpressionParser
-opp.TermParser <- pTerm <|> between (pChar '(') (pChar ')' .>> spaces) pExpression
-opp.AddOperator(InfixOperator("*", spaces, 1, Associativity.Left, (*)))
-opp.AddOperator(InfixOperator("+", spaces, 1, Associativity.Left, (+)))
+let expressionParser (additionPrecedence, multiplyPrecedence) =
+    let pTerm = puint64 .>> spaces
+    let pChar c = pchar c .>> spaces
+    let opp = OperatorPrecedenceParser<_, _, _>()
+    let pExpression = opp.ExpressionParser
+    opp.TermParser <- pTerm <|> between (pChar '(') (pChar ')' .>> spaces) pExpression
+    opp.AddOperator(InfixOperator("+", spaces, additionPrecedence, Associativity.Left, (+)))
+    opp.AddOperator(InfixOperator("*", spaces, multiplyPrecedence, Associativity.Left, (*)))        
+    pExpression
 
-let evaluate line =
-    match run pExpression line with
-    | Success (expression, _, _) -> expression
-    | Failure _ -> failwith "Invalid expression"
+let evaluateEquation precedence =
+    let pExpression = expressionParser precedence 
+    
+    fun line ->
+        match run pExpression line with
+        | Success (expression, _, _) -> expression
+        | Failure _ -> failwith "Invalid expression"
+        
+let sumEquations precedence = Seq.sumBy (evaluateEquation precedence) equations 
 
-let part1 = Seq.sumBy evaluate equations
-
-let part2 = 0
+let part1 = sumEquations (1, 1)
+let part2 = sumEquations (2, 1)
 
 let solution = part1, part2
