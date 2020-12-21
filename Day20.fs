d4e547db7fbf05e42ef4febd85c387fd9d40e405
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-21 13:04:15 +0100

    Work on day 20

--- Day20.fs
+++ Day20.fs
@@ -1,35 +1,33 @@
 ﻿module AdventOfCode.Day20
 
-
-module Array2D =
-    let rows arr = Array.init (Array2D.length1 arr) (fun row -> arr.[row, *])
-    let cols arr = Array.init (Array2D.length2 arr) (fun col -> arr.[*, col])    
+module Array2D =    
     let rotate arr = Array2D.init (Array2D.length2 arr) (Array2D.length1 arr) (fun row col -> Array2D.get arr (Array2D.length1 arr - col - 1) row)
 
-let linedUp (tiles: char [,] [,]) =
-    let linedUpVertically =    
+let linedUp (tiles: (int * char [,]) [] []) =
+    let linedUpVertically =
         tiles
-            |> Array2D.rows
-            |> Array.pairwise
-            |> Array.forall (fun (left, right) -> left.[*].[^0] = right.[*].[0])
-            
-    let linedUpHorizontally =    
+        |> Array.pairwise
+        |> Array.forall (fun (left, right) ->
+            let a = left.[*].[^0]
+            left.[*].[^0] = right.[*].[0]
+        )
+        
+    let linedUpHorizontally =
         tiles
-            |> Array2D.cols
-            |> Array.pairwise            
-            |> Array.forall (fun (top, bottom) -> top.[^0].[*] = bottom.[0].[*])
-            
+        |> Array.transpose
+        |> Array.pairwise
+        |> Array.forall (fun (left, right) -> left.[*].[^0] = right.[*].[0])
+
     linedUpVertically && linedUpHorizontally
 
 let tiles =
     let tiles =
         Input.asLines 20
         |> Array.chunkBySize 12
-        |> Array.map (fun lines -> Array2D.init 10 10 (fun row col -> lines.[row + 1].[col]))
-//        |> Array.map (fun lines -> int lines.[0].[5..8], Array2D.init 10 10 (fun row col -> lines.[row + 1].[col]))
+        |> Array.map (fun lines -> int lines.[0].[5..8], Array2D.init 10 10 (fun row col -> lines.[row + 1].[col]))
 
     let dimension = tiles.Length |> float |> sqrt |> int    
-    Array2D.init dimension dimension (fun row col -> tiles.[row * dimension + col])
+    Array.init dimension (fun row -> Array.init dimension (fun col -> tiles.[row * dimension + col]))
 
 printfn "%A" (linedUp tiles)
 
