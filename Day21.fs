58c512c2bff8dfcf3bf27da18aa0d586fb15eed0
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-21 16:35:58 +0100

    Solve day 21 part 1

--- Day21.fs
+++ Day21.fs
@@ -1,27 +1,42 @@
 ﻿module AdventOfCode.Day21
 
-open System.Text.RegularExpressions
-
-let parseFood history (line: string) =
+let parseFood (line: string) =
     let line = line.Replace("(", "").Replace(")", "").Replace(",", "")
+    let words (str: string) = str.Split(" ") |> set
+       
+    match line.Split(" contains ") |> Array.toList with
+    | [ingredientsStr] -> words ingredientsStr, Set.empty  
+    | [ingredientsStr; allergensStr] -> words ingredientsStr, words allergensStr
+    | _ -> failwith "Invalid line"
+
+let foods = Input.asLines 21 |> Array.map parseFood
     
-    let ingredients, allergens =        
-        match line.Split(" contains ") |> Array.toList with
-        | [ingredientsStr] -> ingredientsStr.Split(" "), Array.empty  
-        | [ingredientsStr; allergensStr] -> ingredientsStr.Split(" "), allergensStr.Split(" ")
-        | _ -> failwith "Invalid line"
-
-    allergens
-    |> Seq.fold (fun acc allergen ->
-        match Map.tryFind allergen acc with
-        | Some history -> Map.add allergen (ingredients :: history) acc
-        | None -> Map.add allergen [ingredients] acc
-    ) history
-
 let part1 =
-    Input.asLines 21
-    |> Array.fold parseFood Map.empty
-    |> printfn "%A"
+    let allergenIngredients = 
+    
+        foods
+        |> Seq.fold (fun acc (ingredients, allergens) ->
+            allergens
+            |> Seq.fold (fun acc allergen ->
+                match Map.tryFind allergen acc with
+                | Some history -> Map.add allergen (ingredients :: history) acc
+                | None -> Map.add allergen [ingredients] acc
+            ) acc
+        ) Map.empty
+        |> Map.map (fun _ ingredients -> ingredients |> List.reduce Set.intersect)
+        |> Map.toSeq
+        |> Seq.map snd
+        |> Seq.reduce Set.union
+
+    let nonAllergenIngredients =
+        foods
+        |> Seq.collect fst
+        |> Seq.except allergenIngredients
+    
+    foods
+    |> Seq.collect fst
+    |> Seq.filter (fun ingredient -> Seq.contains ingredient nonAllergenIngredients)
+    |> Seq.length
     
 let part2 = 0
 
