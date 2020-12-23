0a4769b45112867c298094fb199c55eb03b58b18
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-23 18:23:53 +0100

    Work

--- Day23.fs
+++ Day23.fs
@@ -6,43 +6,51 @@ let initialCups = [3; 8; 9; 1; 2; 5; 4; 6; 7]
 //let initialCups = [5; 8; 3; 9; 7; 6; 2; 4; 1]
 
 let applyMoves count cups =
-//    LinkedList()
-    
-    let rec applyMove round cups =
+    let rec applyMove round (current: LinkedListNode<int>) (ordering: LinkedList<int>) =
         if round = count then
-            cups
-        else        
-            match cups with
-            | current::remainder ->
-                let pickUp = remainder.[0..2]
-                                
-                let destination =
-                    remainder.[3..]
-                    |> List.sortDescending
-                    |> List.partition ((>) current)
-                    |> fun (smaller, larger) -> smaller @ larger
-                    |> List.head
-                
-                let destinationIndex = List.findIndex ((=) destination) remainder
-                let beforeDestination = remainder.[3..destinationIndex]
-                let afterDestination = remainder.[destinationIndex + 1..]
-                
-                let reordered = beforeDestination @ pickUp @ afterDestination @ [current]
+            ordering
+        else
+            let pickUp = [current.Next; current.Next.Next; current.Next.Next.Next]
+            
+            printfn "move: %d" round        
+            printfn "cups: (%d) %s" current.Value (ordering |> Seq.cast<int> |> Seq.tail |> Seq.map string |> String.concat " ")
+            printfn "pick up: %s" (pickUp |> Seq.map (fun elem -> elem.Value |> string) |> String.concat " ")
+//            printfn "destination: %A" destination
+            
+//            match cups with
+//            | current::remainder ->
+//                let pickUp = remainder.[0..2]
+//                                
+//                let destination =
+//                    remainder.[3..]
+//                    |> List.sortDescending
+//                    |> List.partition ((>) current)
+//                    |> fun (smaller, larger) -> smaller @ larger
+//                    |> List.head
+//                
+//                let destinationIndex = List.findIndex ((=) destination) remainder
+//                let beforeDestination = remainder.[3..destinationIndex]
+//                let afterDestination = remainder.[destinationIndex + 1..]
+//                
+//                let reordered = beforeDestination @ pickUp @ afterDestination @ [current]
 
-//                printfn "move: %A" round        
-//                printfn "cups: (%A) %s" current (remainder |> Seq.map string |> String.concat " ")
-//                printfn "pick up: %s" (pickUp |> Seq.map string |> String.concat " ")
-//                printfn "destination: %A" destination
+
                 
-                applyMove (round + 1) reordered
-            | _ -> failwith "Should not occur"
+            // TODO
+            applyMove (round + 1) current ordering
     
-    let finalOrdering = applyMove 0 cups
-    let oneIndex = finalOrdering |> List.findIndex ((=) 1)
-    finalOrdering.[oneIndex + 1..] @ finalOrdering.[0..oneIndex - 1]
+    let startOrdering = LinkedList(cups)
+    let finalOrdering = applyMove 0 startOrdering.First startOrdering
+    printfn "%A" finalOrdering
+    
+    finalOrdering
+//    let finalOrdering = applyMove 0 cups
+//    let oneIndex = finalOrdering |> List.findIndex ((=) 1)
+//    finalOrdering.[oneIndex + 1..] @ finalOrdering.[0..oneIndex - 1]
 
 let part1 =
-    applyMoves 100 initialCups    
+    applyMoves 10 initialCups    
+//    applyMoves 100 initialCups    
     |> Seq.map string |> Seq.toArray |> String.concat ""
 
 let part2 =
