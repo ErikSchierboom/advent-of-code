eb06f79a2d83513a6032b05f20720446003c301e
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-05 10:40:16 +0100

    Enable all days

--- Program.fs
+++ Program.fs
@@ -2,30 +2,29 @@
 
 [<EntryPoint>]
 let main _ =
-//    printfn "Day  1: %A" Day1.solution
-//    printfn "Day  2: %A" Day2.solution
-//    printfn "Day  3: %A" Day3.solution
-//    printfn "Day  4: %A" Day4.solution
-//    printfn "Day  5: %A" Day5.solution
-//    printfn "Day  6: %A" Day6.solution
-//    printfn "Day  7: %A" Day7.solution
-//    printfn "Day  8: %A" Day8.solution
-//    printfn "Day  9: %A" Day9.solution
-//    printfn "Day 10: %A" Day10.solution
-//    printfn "Day 11: %A" Day11.solution
-//    printfn "Day 12: %A" Day12.solution
-//    printfn "Day 13: %A" Day13.solution
-//    printfn "Day 14: %A" Day14.solution
-//    printfn "Day 15: %A" Day15.solution
-//    printfn "Day 16: %A" Day16.solution
-//    printfn "Day 17: %A" Day17.solution
-//    printfn "Day 18: %A" Day18.solution
-//    printfn "Day 19: %A" Day19.solution
-//    printfn "Day 20: %A" Day20.solution
-//    printfn "Day 21: %A" Day21.solution
-//    printfn "Day 22: %A" Day22.solution
-//    printfn "Day 23: %A" Day23.solution
-//    printfn "Day 25: %A" Day25.solution
+    printfn "Day  1: %A" Day1.solution
+    printfn "Day  2: %A" Day2.solution
+    printfn "Day  3: %A" Day3.solution
+    printfn "Day  4: %A" Day4.solution
+    printfn "Day  5: %A" Day5.solution
+    printfn "Day  6: %A" Day6.solution
+    printfn "Day  7: %A" Day7.solution
+    printfn "Day  8: %A" Day8.solution
+    printfn "Day  9: %A" Day9.solution
+    printfn "Day 10: %A" Day10.solution
+    printfn "Day 11: %A" Day11.solution
+    printfn "Day 12: %A" Day12.solution
+    printfn "Day 13: %A" Day13.solution
+    printfn "Day 14: %A" Day14.solution
+    printfn "Day 15: %A" Day15.solution
+    printfn "Day 16: %A" Day16.solution
+    printfn "Day 17: %A" Day17.solution
+    printfn "Day 18: %A" Day18.solution
+    printfn "Day 19: %A" Day19.solution
+    printfn "Day 20: %A" Day20.solution
+    printfn "Day 21: %A" Day21.solution
+    printfn "Day 22: %A" Day22.solution
+    printfn "Day 23: %A" Day23.solution
     printfn "Day 25: %A" Day25.solution
 
     0
