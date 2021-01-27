b91b3f7ec26b62d4a3a04f1a2c2981cfa02e5673
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-27 11:44:49 +0100

    [2019] Solve 1.2

--- 2019/day1.nim
+++ 2019/day1.nim
@@ -2,14 +2,21 @@ import input, strformat
 
 let masses = readInputAsInts(1)  
 
-func fuel(mass: int): int = mass div 3 + 2
+func fuelForMass(mass: int): int = mass div 3 - 2
+
+func fuelForFuel(mass: int): int =
+  var current = mass
+
+  while current > 0:
+    current = fuelForMass(current)
+    result.inc(current)
 
 proc part1(): int =
   for mass in masses:
-    result += fuel(mass)
+    result += fuelForMass(mass)
 
 proc part2(): int =
   for mass in masses:
-    discard
+    result += fuelForFuel(mass)
 
 echo &"day 1: {part1()}, {part2()}"
