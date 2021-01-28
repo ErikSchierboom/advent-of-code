2581d0d5fd6b0ae0ad6bf21aee3d41b7a10b9fc5
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-28 20:50:44 +0100

    [2019] Solve 3.2

--- 2019/day3.nim
+++ 2019/day3.nim
@@ -1,50 +1,39 @@
-import input, sets, strformat, strutils
+import input, sets, sequtils, strformat, strutils, tables
 
 let lines = readInputAsLines(3)
 
 func manhattanDistance(fst: (int, int)): int =
   abs(fst[0]) + abs(fst[1])
 
-func coordsForPath(instructions: seq[string]): HashSet[(int, int)] =
+func coordsForPath(instructions: seq[string]): Table[(int, int), int] =
   var current = (0, 0)
+  var steps = 0
 
   for instruction in instructions:
     let direction = instruction[0]
     let offset = parseInt(instruction[1..^1])
-    let offsetRange = 0 ..< offset
-    case direction
-      of 'R': 
-        for dx in offsetRange:
-          current[0].inc()
-          result.incl(current)
-      of 'L': 
-        for dx in offsetRange:
-          current[0].dec()
-          result.incl(current)
-      of 'U':
-        for dy in offsetRange:
-          current[1].inc()
-          result.incl(current)
-      of 'D':
-        for dy in offsetRange:
-          current[1].dec()
-          result.incl(current)
+
+    for dx in 0 ..< offset:
+      case direction
+      of 'R': current[0].inc()
+      of 'L': current[0].dec()
+      of 'U': current[1].inc()
+      of 'D': current[1].dec()
       else: quit(1)
 
-  result.excl((0, 0))
+      steps.inc()
+      discard result.hasKeyOrPut(current, steps)
 
-proc coordsForWire(index: int): HashSet[(int, int)] =
+proc coordsForWire(index: int): Table[(int, int), int] =
   coordsForPath(lines[index].split(','))
 
 let wire1Coords = coordsForWire(0)
 let wire2Coords = coordsForWire(1)
 
-proc part1(): int = 
-  result = high(int)
-  for coord in wire1Coords * wire2Coords:
-    result = min(result, coord.manhattanDistance)
+let crossingCoords = toHashSet(toSeq(wire1Coords.keys)) * toHashSet(toSeq(wire2Coords.keys))
 
-proc part2(): int =
-  discard
+proc part1(): int = crossingCoords.mapIt(it.manhattanDistance).min
+
+proc part2(): int = crossingCoords.mapIt(wire1Coords[it] + wire2Coords[it]).min
 
 echo &"day 3: {part1()}, {part2()}"
