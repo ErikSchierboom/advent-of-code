eeee416f1963a37c656fc4b0208e6a576982672f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-01 08:20:40 +0100

    2021/1: part 2

--- 2021/day1.nim
+++ 2021/day1.nim
@@ -1,10 +1,17 @@
+import std/[math,strformat]
 import input
 
 let inputs = readInputInts(Day(1))
 
 proc part1: int =
-  for i in 1 .. inputs.high:
-    if inputs[i] > inputs[i - 1]:
+  for i in 0 ..< inputs.high:
+    if inputs[i + 1] > inputs[i]:
       inc result
 
-echo $part1()
+proc part2: int =
+  for i in 0 ..< inputs.high - 2:
+    if sum(inputs[i + 1 .. i + 3]) > sum(inputs[i .. i + 2]):
+      inc result
+
+echo &"1.1: {part1()}"
+echo &"1.2: {part2()}"
