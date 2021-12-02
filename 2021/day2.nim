50d87cda33153c9d6799f90029df639f6bce3134
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-02 13:56:40 +0100

    2021/2: solve part 2

--- 2021/day2.nim
+++ 2021/day2.nim
@@ -1,8 +1,15 @@
 import aoc, std/strscans
 
+type 
+  Submarine = object
+    x: int
+    y: int
+    aim: int
+
 proc solve(input: string): IntSolution =
   var
-    point: Point
+    sub1: Submarine
+    sub2: Submarine
     direction: string
     distance: int
 
@@ -10,12 +17,17 @@ proc solve(input: string): IntSolution =
     if line.scanf("$w $i", direction, distance):
       case direction:
         of "forward":
-          point.x += distance
+          sub1.x += distance
+          sub2.x += distance
+          sub2.y += sub2.aim * distance
         of "up":
-          point.y -= distance
+          sub1.y -= distance
+          sub2.aim -= distance
         of "down":
-          point.y += distance
+          sub1.y += distance          
+          sub2.aim += distance
 
-  result.part1 = point.x * point.y
+  result.part1 = sub1.x * sub1.y
+  result.part2 = sub2.x * sub2.y
 
 echo solve("input/day2.txt")
