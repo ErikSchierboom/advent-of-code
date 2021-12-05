20953715a6fe1bf9182aa74541bb95510d7a4b1f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-05 12:40:59 +0100

    2021/2: assume success

--- 2021/src/day02.nim
+++ 2021/src/day02.nim
@@ -3,16 +3,15 @@ import helpers, std/strscans
 proc solveDay2*: IntSolution =
   var sub: tuple[x, y, aim: int]
 
-  for (success, command, distance) in readInputScans(day = 2, pattern = "$w $i"):
-    if success:
-      case command:
-        of "forward":
-          sub.x += distance
-          sub.y += sub.aim * distance
-        of "up":
-          sub.aim -= distance
-        of "down":
-          sub.aim += distance
+  for (_, command, distance) in readInputScans(day = 2, pattern = "$w $i"):
+    case command:
+      of "forward":
+        sub.x += distance
+        sub.y += sub.aim * distance
+      of "up":
+        sub.aim -= distance
+      of "down":
+        sub.aim += distance
 
   result.part1 = sub.x * sub.aim
   result.part2 = sub.x * sub.y
