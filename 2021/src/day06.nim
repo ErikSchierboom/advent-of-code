babbb5af661dc79de91c8653b1c58efd75c1d06d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-06 19:18:09 +0100

    2021/6: part 2

--- 2021/src/day06.nim
+++ 2021/src/day06.nim
@@ -1,21 +1,21 @@
 import helpers, std/[math, sequtils, strutils]
 
-proc readInitialState: array[9, int] =
-  for timer in readInputString(day = 6).split(',').mapIt(parseInt($it)):
+proc readInitialState: array[9, uint64] =
+  for timer in readInputString(day = 6).split(',').mapIt(parseBiggestUInt($it)):
     inc result[timer]
 
-func simulateDay(state: var array[9, int]) =
+func simulateDay(state: var array[9, uint64]) =
   let old = state
 
   for i in countdown(state.high, state.low):
     # TODO: mod
     if i == 0:
       state[8] = old[0]
-      inc state[6], old[0]
+      state[6] += old[0]
     else:
       state[i - 1] = old[i]
 
-proc part1(state: var array[9, int]): int =
+proc part1(state: var array[9, uint64]): uint64 =
   for i in 0..<80:
     echo state
     state.simulateDay()
@@ -23,10 +23,26 @@ proc part1(state: var array[9, int]): int =
   result = state.sum
   echo result
 
-proc solveDay6*: IntSolution =
+proc part2(state: var array[9, uint64]): uint64 =
+  for i in 0..<256:
+    echo state
+    state.simulateDay()
+
+  result = state.sum
+  echo result
+
+proc solveDay6*: Solution[uint64, uint64] =
   var state = readInitialState()
-  result.part1 = part1(state)
-  result.part2 = 1
+  for i in 1..256:
+    state.simulateDay()
+
+    if i == 80:
+      result.part1 = state.sum
+    elif i == 256:
+      result.part2 = state.sum
+
+  # result.part1 = part1(state)
+  # result.part2 = part2(state)
 
 when isMainModule:
   echo solveDay6()
--- 2021/tests/tday06.nim
+++ 2021/tests/tday06.nim
@@ -3,7 +3,7 @@ import day06, unittest
 suite "day 6":
   test "part 1":
     let solution = solveDay6()
-    check: 5934 == solution.part1
+    check: 355386 == solution.part1
 
   test "part 2":
     let solution = solveDay6()
