3d3e09f74be8602bd8ff0114d09bdd98a4f69cf4
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 08:39:54 +0100

    2021/7: re-use shared logic

--- 2021/src/day07.nim
+++ 2021/src/day07.nim
@@ -2,17 +2,13 @@ import helpers, std/[math, sequtils, sugar]
 
 func summation(upTo: int): int = upTo * (upTo + 1) div 2
 
-func part1(xs: seq[int]): int =
-  let median = xs.median()
-  result = xs.mapIt(abs(it - median)).sum
-
-func part2(xs: seq[int]): int =
-  result = (0 ..< xs.max).mapIt(xs.map((x) => abs(it - x).summation).sum).min()
+func lowestCost(xs: seq[int], calculate: (int, int) -> int): int =
+  result = xs.deduplicate.mapIt(xs.map((x) => calculate(x, it)).sum).min
 
 proc solveDay7*: IntSolution =
   var xs = readInputString(day = 7).splitToInts().toSeq
-  result.part1 = part1(xs)
-  result.part2 = part2(xs)
+  result.part1 = lowestCost(xs, (x, goal) => abs(goal - x))
+  result.part2 = lowestCost(xs, (x, goal) => abs(goal - x).summation)
 
 when isMainModule:
   echo solveDay7()
