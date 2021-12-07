098a7830df9a6f36eceaa580d591e82745dd53e4
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 08:43:53 +0100

    2021/7: refactor

--- 2021/src/day07.nim
+++ 2021/src/day07.nim
@@ -3,12 +3,12 @@ import helpers, std/[math, sequtils, sugar]
 func summation(upTo: int): int = upTo * (upTo + 1) div 2
 
 func lowestCost(xs: seq[int], calculate: (int, int) -> int): int =
-  result = xs.deduplicate.mapIt(xs.map((x) => calculate(x, it)).sum).min
+  result = xs.deduplicate.map((goal) => xs.map((x) => calculate(x, goal)).sum).min
 
 proc solveDay7*: IntSolution =
   var xs = readInputString(day = 7).splitToInts().toSeq
-  result.part1 = lowestCost(xs, (x, goal) => abs(goal - x))
-  result.part2 = lowestCost(xs, (x, goal) => abs(goal - x).summation)
+  result.part1 = lowestCost(xs, (x, goal) => abs(x - goal))
+  result.part2 = lowestCost(xs, (x, goal) => abs(x - goal).summation)
 
 when isMainModule:
   echo solveDay7()
