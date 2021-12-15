bad4728e341953716e0dc6973ccbd5626a4d1af2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-15 12:20:28 +0100

    2021/15: optimize

--- 2021/src/day15.nim
+++ 2021/src/day15.nim
@@ -1,57 +1,52 @@
-import helpers, std/[algorithm, sequtils, sets, tables]
+import helpers, std/[heapqueue, sequtils, tables]
 
-func `[]`(grid: seq[seq[int]], point: Point): int {.inline.} = grid[point.y][point.x]
+type Node = object
+  pos: Point
+  cost: int
 
-func points(grid: seq[seq[int]]): seq[Point] =
+proc `<` (a, b: Node): bool = a.cost < b.cost
+
+func points(grid: var seq[seq[int]]): seq[Point] =
   for y in grid.low .. grid.high:
     for x in grid[0].low .. grid[0].high:
       result.add (x: x, y: y)
 
-func neighbors(grid: seq[seq[int]], point: Point): HashSet[Point] =
-  if point.x > grid[0].low:  result.incl (x: point.x - 1, y: point.y)
-  if point.x < grid[0].high: result.incl (x: point.x + 1, y: point.y)
-  if point.y > grid.low:     result.incl (x: point.x,     y: point.y - 1)
-  if point.y < grid.high:    result.incl (x: point.x,     y: point.y + 1)
-
-proc lowest(q: var HashSet[Point], dist: var Table[Point, int]): Point {.inline.} =
-  var currentLow = high(int)
-
-  for point in q:
-    if dist[point] < currentLow:
-      currentLow = dist[point]
-      result = point
+iterator neighbors(grid: var seq[seq[int]], point: Point): Point =
+  if point.x > grid[0].low:  yield (x: point.x - 1, y: point.y)
+  if point.x < grid[0].high: yield (x: point.x + 1, y: point.y)
+  if point.y > grid.low:     yield (x: point.x,     y: point.y - 1)
+  if point.y < grid.high:    yield (x: point.x,     y: point.y + 1)
 
 proc solveDay15*: IntSolution =
-  let grid = readInputDigits(day = 15).toSeq
-  let points = grid.points
+  var grid = readInputDigits(day = 15).toSeq
+  var points = grid.points
 
   var source = (x: 0, y: 0)
-  var q: HashSet[Point]
-  var dist: Table[Point, int]
-  var neighbors: Table[Point, HashSet[Point]]
+  var q: HeapQueue[Node]
+  var dist = newTable[Point, int]()
   let target = (x: grid[0].high, y: grid.high)
 
   for point in points:
     dist[point] = high(int)
-    neighbors[point] = grid.neighbors(point)
-    q.incl point
 
-  dist[source] = 0
+  dist[source] = 0  
+  q.push Node(pos: source, cost: 0)
   
   while q.len > 0:
-    let u = lowest(q, dist)
-    q.excl u
+    let u = q.pop()
 
-    if u == target:
-      break
+    if u.pos == target:
+      result.part1 = dist[target]
+      return
 
-    for v in neighbors[u]:
-      if v in q:
-        let alt = dist[u] + grid[v]
-        if alt < dist[v]:
-          dist[v] = alt
+    if u.cost > dist[u.pos]:
+      continue
 
-  result.part1 = dist[target]
+    for v in grid.neighbors(u.pos):
+      let next = Node(cost: u.cost + grid[v.y][v.x], pos: v)
+      if next.cost < dist[next.pos]:
+        q.push next
+        dist[next.pos] = next.cost
  
 when isMainModule:
   echo solveDay15()
