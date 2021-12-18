4e7301e7ff1d4b42c102a0cde372a3b55f294831
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-18 19:18:54 +0100

    Work

--- 2021/src/day18.nim
+++ 2021/src/day18.nim
@@ -2,75 +2,66 @@ import helpers, std/[sequtils, strutils]
 
 type
   NumberKind = enum
-    nkNil, nkLiteral, nkPair
+    nkLiteral, nkPair
   Number = ref object
-    parent: Number
     case kind: NumberKind
-      of nkNil: discard
       of nkLiteral: value: int
       of nkPair: left, right: Number
 
 func `$`(number: Number): string =
   case number.kind
-    of nkNil: "nil"
     of nkLiteral: $number.value
     of nkPair: "[" & $number.left & "," & $number.right & "]"
 
 func `+`(left: Number, right: Number): Number =
   Number(kind: nkPair, left: left, right: right)
 
-func depth(number: Number): int = 
-  if number.kind == nkNil: 0 else: 1 + number.parent.depth
-
 proc reduce(number: Number): Number =
-  if number.kind == nkLiteral:
-    return
-  
-    # if depth == 4 and number.right.kind == nkLiteral and number.left.kind == nkPair and number.left.left.kind == nkLiteral and number.left.right.kind == nkLiteral:
-    #   Number(kind: nkPair, left: Number(kind: nkLiteral, value: 0), right: Number(kind: nkLiteral, value: number.right.value + number.left.right.value))
-    # elif depth == 4 and number.left.kind == nkLiteral and number.right.kind == nkPair and number.right.left.kind == nkLiteral and number.right.right.kind == nkLiteral:
-    #   Number(kind: nkPair, left: Number(kind: nkLiteral, value: number.left.value + number.right.left.value), right: Number(kind: nkLiteral, value: 0))
-    # else:
-    #   Number(kind: nkPair, left: reduce(number.left, depth + 1), right: reduce(number.right, depth + 1))
+  proc inner(number: Number, depth: int): Number =
+    case number.kind
+      of nkLiteral:
+        number
+      of nkPair:
+        Number(kind: nkPair, left: inner(number.left, depth + 1), right: inner(number.right, depth + 1))
 
-proc parseNumber(line: string, index: var int, parent: Number): Number
+  inner(number, 0)
 
-proc parsePair(line: string, index: var int, parent: Number): Number =
-  result = Number(kind: nkPair, parent: parent)
+proc parseNumber(line: string, index: var int): Number
+
+proc parsePair(line: string, index: var int): Number =
+  result = Number(kind: nkPair)
   inc index # '['
-  result.left = parseNumber(line, index, result)
+  result.left = parseNumber(line, index)
   inc index # ','
-  result.right = parseNumber(line, index, result)
+  result.right = parseNumber(line, index)
   inc index # ']'
 
-proc parseLiteral(line: string, index: var int, parent: Number): Number =
+proc parseLiteral(line: string, index: var int): Number =
   var value: int
 
   while line[index].isDigit:
     value = value * 10 + parseInt($line[index])
     inc index
 
-  Number(kind: nkLiteral, value: value, parent: parent)
+  Number(kind: nkLiteral, value: value)
 
-proc parseNumber(line: string, index: var int, parent: Number): Number =
+proc parseNumber(line: string, index: var int): Number =
   if line[index] == '[':
-    parsePair(line, index, parent)
+    parsePair(line, index)
   else:
-    parseLiteral(line, index, parent)
+    parseLiteral(line, index)
 
 proc readInputEquation: Number =
   var numbers: seq[Number]
   for line in readInputStrings(day = 18):
     var index: int
-    numbers.add parseNumber(line, index, Number(kind: nkNil))
+    numbers.add parseNumber(line, index)
 
   numbers.foldl(a + b)
 
 proc solveDay18*: IntSolution =
-  let equation = readInputEquation()
-  echo equation.depth
-  echo equation.left.left.depth
-  # equation.reduce
+  var equation = readInputEquation()
+  echo equation.reduce()
  
 when isMainModule:
   echo solveDay18()
