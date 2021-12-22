94111e0c6fa7f65d1b62c0b3231bb70f9f5498e1
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-22 19:17:26 +0100

    Refactor

--- 2021/src/day20.nim
+++ 2021/src/day20.nim
@@ -1,30 +1,28 @@
 import helpers, std/[sequtils, sets]
 
-const lightPixel = '#'
-
-iterator grid(point: Point): Point =
-  for dy in -1 .. 1:
-    for dx in -1 .. 1:
+iterator square(point: Point, offset: int): Point =
+  for dy in -offset .. offset:
+    for dx in -offset .. offset:
       yield (x: point.x + dx, y: point.y + dy)
 
 proc index(points: HashSet[Point], point: Point): int =
-  for gridPoint in point.grid:
+  for gridPoint in point.square(offset = 1):
     result = result shl 1 or (if gridPoint in points: 1 else: 0)
 
 proc parseEnhancementAlgorithm(line: string): HashSet[int] =
   for i in line.low .. line.high:
-    if line[i] == lightPixel:
+    if line[i] == '#':
       result.incl(i)
 
 proc parseInputImage(lines: seq[string]): HashSet[Point] =
   for y in lines.low .. lines.high:
     for x in lines[0].low .. lines[0].high:
-      if lines[y][x] == lightPixel:
+      if lines[y][x] == '#':
         result.incl (x: x, y: y)
 
 proc enhance(points: HashSet[Point], enhancementAlgorithm: HashSet[int]): HashSet[Point] =
   for point in points:
-    for gridPoint in point.grid:
+    for gridPoint in point.square(offset = 2):
       if points.index(gridPoint) in enhancementAlgorithm:
         result.incl gridPoint
 
