49a5bfdcf8a08eea39e69d7b0d5e1fa66927507a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-22 19:42:40 +0100

    Refactor

--- 2021/src/day20.nim
+++ 2021/src/day20.nim
@@ -9,30 +9,30 @@ proc index(points: HashSet[Point], point: Point): int =
   for gridPoint in point.square(offset = 1):
     result = result shl 1 or (if gridPoint in points: 1 else: 0)
 
-proc parseEnhancementAlgorithm(line: string): HashSet[int] =
+proc parseLightPixelEncoding(line: string): HashSet[int] =
   for i in line.low .. line.high:
     if line[i] == '#':
       result.incl(i)
 
-proc parseInputImage(lines: seq[string]): HashSet[Point] =
+proc parseLightPixels(lines: seq[string]): HashSet[Point] =
   for y in lines.low .. lines.high:
     for x in lines[0].low .. lines[0].high:
       if lines[y][x] == '#':
         result.incl (x: x, y: y)
 
-proc enhance(points: HashSet[Point], enhancementAlgorithm: HashSet[int]): HashSet[Point] =
+proc enhance(points: HashSet[Point], lightPixelEncoding: HashSet[int]): HashSet[Point] =
   for point in points:
     for gridPoint in point.square(offset = 2):
-      if points.index(gridPoint) in enhancementAlgorithm:
+      if points.index(gridPoint) in lightPixelEncoding:
         result.incl gridPoint
 
 proc solveDay20*: IntSolution =
   let lines = readInputStrings(day = 20).toSeq
-  let enhancementAlgorithm = parseEnhancementAlgorithm(lines[0])
-  var lightPixels = parseInputImage(lines[2..^1])
+  let lightPixelEncoding = parseLightPixelEncoding(lines[0])
+  var lightPixels = parseLightPixels(lines[2..^1])
 
-  lightPixels = lightPixels.enhance(enhancementAlgorithm)
-  lightPixels = lightPixels.enhance(enhancementAlgorithm)
+  lightPixels = lightPixels.enhance(lightPixelEncoding)
+  lightPixels = lightPixels.enhance(lightPixelEncoding)
 
   result.part1 = lightPixels.len
   # result.part2 = part2(numbers)
