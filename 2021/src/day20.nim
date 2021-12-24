e5d9b5d8f6fb74e5d575779d66d971353ef71702
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-24 10:34:52 +0100

    Working

--- 2021/src/day20.nim
+++ 2021/src/day20.nim
@@ -1,60 +1,62 @@
-import helpers, std/[sequtils, sets]
+import helpers, std/sequtils
 
-type Grid = tuple[minX, maxX, minY, maxY, unknown: int, pixels: HashSet[Point], encoding: HashSet[int]]
+const lightPixel = '#'
+const darkPixel = '.'
+
+type Grid = tuple[unknown: char, pixels: seq[seq[char]], encoding: string]
 
 iterator square(point: Point): Point =
   for dy in -1 .. 1:
     for dx in -1 .. 1:
       yield (x: point.x + dx, y: point.y + dy)
 
-func `notin`(point: Point, grid: Grid): bool =
-  point.x < grid.minX or point.x > grid.maxX or
-  point.y < grid.minY or point.y > grid.maxY
+func bit(c: char): int {.inline.} =
+  if c == lightPixel: 1 else: 0
 
-proc index(grid: Grid, point: Point): int =
+func index(grid: Grid, point: Point): int =
   for gridPoint in point.square:
     result = result shl 1
-    if gridPoint notin grid and grid.unknown in grid.encoding:
-      result = result or 1
-    elif gridPoint in grid.pixels:
-      result = result or 1
+    if gridPoint.x < grid.pixels[0].low or gridPoint.x > grid.pixels[0].high or
+       gridPoint.y < grid.pixels.low or gridPoint.y > grid.pixels.high:
+      result = result or grid.unknown.bit
+    else:
+      result = result or grid.pixels[gridPoint.y][gridPoint.x].bit
 
-proc parseEncoding(line: string): HashSet[int] =
-  for i in line.low .. line.high:
-    if line[i] == '#':
-      result.incl(i)
+func enhance(grid: Grid): Grid =
+  result.encoding = grid.encoding
+  result.pixels = newSeq[seq[char]](grid.pixels.len + 2)
 
-proc parsePixels(lines: seq[string]): HashSet[Point] =
+  for y in grid.pixels.low - 1 .. grid.pixels.high + 1:
+    result.pixels[y + 1] = newSeq[char](grid.pixels[0].len + 2)
+    for x in grid.pixels[0].low - 1 .. grid.pixels[0].high + 1:
+      let point = (x: x, y: y)
+      result.pixels[y + 1][x + 1] = grid.encoding[grid.index(point)]
+
+  result.unknown = if grid.unknown == darkPixel: lightPixel else: darkPixel
+
+func numLightPixels(grid: Grid): int =
+  for row in grid.pixels:
+    for col in row:
+      if col == lightPixel:
+        inc result
+
+proc parsePixels(lines: seq[string]): seq[seq[char]] =
   for y in lines.low .. lines.high:
-    for x in lines[0].low .. lines[0].high:
-      if lines[y][x] == '#':
-        result.incl (x: x, y: y)
+    result.add lines[y].toSeq
 
 proc parseGrid: Grid =
   let lines = readInputStrings(day = 20).toSeq
-  let imageLines = lines[2..^1]
-  (minX: 0, maxX: imageLines[2].high, minY: 0, maxY: imageLines.high, unknown: 0, pixels: parsePixels(imageLines), encoding: parseEncoding(lines[0]))
-
-proc enhance(grid: Grid): Grid =
-  result.minX = grid.minX - 2; result.maxX = grid.maxX + 2
-  result.minY = grid.minY - 2; result.maxY = grid.maxY + 2
-  result.encoding = grid.encoding
-
-  for y in result.minY .. result.maxY:
-    for x in result.minX .. result.maxX:
-      let point = (x: x, y: y)
-      if grid.index(point) in grid.encoding:
-        result.pixels.incl point
-
-  result.unknown = if result.unknown == 0: 511 else: 0
+  (unknown: darkPixel, pixels: parsePixels(lines[2..^1]), encoding: lines[0])
 
 proc solveDay20*: IntSolution =
   var grid = parseGrid()
-  grid = grid.enhance()
-  grid = grid.enhance()
 
-  result.part1 = grid.pixels.len
-  # result.part2 = part2(numbers)
+  for i in 1..50:
+    grid = grid.enhance()
+    if i == 2:
+      result.part1 = grid.numLightPixels
+    elif i == 50:
+      result.part2 = grid.numLightPixels
  
 when isMainModule:
   echo solveDay20()
--- 2021/tests/tday20.nim
+++ 2021/tests/tday20.nim
@@ -3,8 +3,8 @@ import day20, std/unittest
 suite "day 20":
   test "part 1":
     let solution = solveDay20()
-    check: 3981 == solution.part1
+    check: 5571 == solution.part1
 
   test "part 2":
     let solution = solveDay20()
-    check: 4687 == solution.part2
+    check: 17965 == solution.part2
