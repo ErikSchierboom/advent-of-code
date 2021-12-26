6dfc39571d0d14eba3400b4925715b239a8b5c8f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-26 20:34:41 +0100

    Work

--- 2021/src/day21.nim
+++ 2021/src/day21.nim
@@ -4,10 +4,12 @@ type
   Player = tuple[pos, score: int]
   Game = tuple[p1, p2: Player, round, winningScore: int]
 
-func move(round: int): int = (round * 9 - 3) mod 10
-func won(game: Game, player: Player): bool = player.score >= game.winningScore
-func won(game: Game): bool = game.won(game.p1) or game.won(game.p2)
-func modulo(x, y, offset: int): int = ((x - offset) mod y) + offset
+func modulo(x, y, offset: int): int {.inline.} = ((x - offset) mod y) + offset
+func won(game: Game, player: Player): bool {.inline.} = player.score >= game.winningScore
+func won(game: Game): bool {.inline.} = game.won(game.p1) or game.won(game.p2)
+func move(game: Game): int {.inline.} = (game.round.pred * 3) mod 10
+func move(player: Player, steps: int): int {.inline.} = modulo(player.pos + steps, 10, 1)
+func player1Turn(game: Game): bool {.inline.} = (game.round div 3) mod 2 == 1
 
 proc parseInputGame(winningScore: int): Game =
   let (_, p1Pos, p2Pos) = readInputString(day = 21).scanTuple("Player 1 starting position: $i\nPlayer 2 starting position: $i")
@@ -16,60 +18,45 @@ proc parseInputGame(winningScore: int): Game =
 proc part1: int =
   var game = parseInputGame(winningScore = 1000)
   while not game.won:
-    inc game.round
-
-    if game.round mod 2 == 1:
-      game.p1.pos = modulo(game.p1.pos + move(game.round), 10, 1)
+    inc game.round, 3
+    
+    if game.player1Turn:
+      game.p1.pos = move(game.p1, game.move)
       inc game.p1.score, game.p1.pos
     else:
-      game.p2.pos = modulo(game.p2.pos + move(game.round), 10, 1)
+      game.p2.pos = move(game.p2, game.move)
       inc game.p2.score, game.p2.pos
 
-  if game.won(game.p1):
-    result = game.p2.score * game.round * 3
-  else:
-    result = game.p1.score * game.round * 3
+  result = if game.won(game.p1): game.p2.score * game.round else: game.p1.score * game.round
 
-proc part2: int64 =
-  proc playUntilWon(game: Game, rolls: seq[int]): tuple[p1Wins, p2Wins: int64] =
-    if game.won(game.p1):
-      inc result.p1Wins
-    elif game.won(game.p2):
-      inc result.p2Wins
-    else:
-      for roll in 1..3:
-        var newGame = game
-        var newRolls = rolls
-        inc newGame.round
-        newRolls.add roll
+# proc part2: int64 =
+#   var p1Wins, p2Wins: int64
+#   var oldP1Pos, oldP1Score, oldP2Pos, oldP2Score: int
+#   var rolls: seq[int]
 
-        if newRolls.len == 3:
-          if ((newGame.round - 1) div 3) mod 2 == 0:
-            # echo "p1"
-            
-            newGame.p1.pos = modulo(newGame.p1.pos + newRolls.sum, 10, 1)
-            inc newGame.p1.score, newGame.p1.pos
-            # echo newGame
-          else:
-            # echo "p2"
-            newGame.p2.pos = modulo(newGame.p2.pos + newRolls.sum, 10, 1)
-            inc newGame.p2.score, newGame.p2.pos
+#   var game = parseInputGame(winningScore = 21)
+#   while not game.won:
+#     inc game.round
 
-            # echo newGame
 
-          newRolls = @[]
+#     if newRolls.len == 3:
+#           if ((game.round - 1) div 3) mod 2 == 0:
 
-        let gameResult = playUntilWon(newGame, newRolls)
-        result.p1Wins = result.p1Wins + gameResult.p1Wins
-        result.p2Wins = result.p2Wins + gameResult.p2Wins
+#     oldP1Pos = game.p1.pos; oldP1Score = game.p1.score
+#     oldP2Pos = game.p2.pos; oldP2Score = game.p1.score
 
-  let game = parseInputGame(winningScore = 21)
-  let endResult = game.playUntilWon(rolls = @[])
-  result = if endResult.p1Wins > endResult.p2Wins: endResult.p1Wins else: endResult.p2Wins
+#     if game.round mod 2 == 1:
+#       game.p1.pos = modulo(game.p1.pos + move, 10, 1)
+#       inc game.p1.score, game.p1.pos
+#     else:
+#       game.p2.pos = modulo(game.p2.pos + move, 10, 1)
+#       inc game.p2.score, game.p2.pos
+
+#   result = if p1Wins > p2Wins: p1Wins else: p2Wins
 
 proc solveDay21*: Solution[int, int64] =
   result.part1 = part1()
-  result.part2 = part2()
+  # result.part2 = part2()
 
 when isMainModule:
   echo solveDay21()
