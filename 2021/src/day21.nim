39b03c331cbfef8a9b77e5d012150cc51b667265
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-29 09:26:26 +0100

    Work

--- 2021/src/day21.nim
+++ 2021/src/day21.nim
@@ -1,30 +1,27 @@
 import helpers, std/[math, strscans, tables]
 
-type 
-  Player = tuple[pos, score: int]
-  Game = tuple[p1, p2: Player, round, winningScore: int]
+type Game = tuple[p1Pos, p1Score, p2Pos, p2Score, round: int]
 
-func won(game: Game, player: Player): bool {.inline.} = player.score >= game.winningScore
-func move(player: Player, steps: int): int {.inline.} = floorMod((player.pos + steps - 1), 10) + 1
+func move(pos, steps: int): int {.inline.} = floorMod((pos + steps - 1), 10) + 1
 func player1Turn(game: Game): bool {.inline.} = (game.round div 3) mod 2 == 1
 
-proc parseInputGame(winningScore: int): Game =
+proc parseInputGame: Game =
   let (_, p1Pos, p2Pos) = readInputString(day = 21).scanTuple("Player 1 starting position: $i\nPlayer 2 starting position: $i")
-  (p1: (pos: p1Pos, score: 0), p2: (pos: p2Pos, score: 0), round: 0, winningScore: winningScore)
+  (p1Pos: p1Pos, p1Score: 0, p2Pos: p2Pos, p2Score: 0, round: 0)
 
 proc part1: int =
-  var game = parseInputGame(winningScore = 1000)
-  while not game.won(game.p1) or game.won(game.p2):
-    inc game.round, 3
+  var (p1Pos, p1Score, p2Pos, p2Score, round) = parseInputGame()
+  while p1Score < 1000 and p2Score < 1000:
+    inc round
     
-    if game.player1Turn:
-      game.p1.pos = move(game.p1, game.round.pred * 3)
-      inc game.p1.score, game.p1.pos
+    if round mod 2 == 1:
+      p1Pos = move(p1Pos, (round * 3).pred * 3)
+      inc p1Score, p1Pos
     else:
-      game.p2.pos = move(game.p2, game.round.pred * 3)
-      inc game.p2.score, game.p2.pos
+      p2Pos = move(p2Pos, (round * 3).pred * 3)
+      inc p2Score, p2Pos
 
-  result = min(game.p1.score, game.p2.score) * game.round
+  result = min(p1Score, p2Score) * round * 3
 
 proc part2: int64 =
   const moveFreqs = {3:1, 4:3, 5:6, 6:7, 7:6, 8:3, 9:1}.toTable
@@ -35,28 +32,28 @@ proc part2: int64 =
       inc newGame.round, 3
 
       if newGame.player1Turn:
-        newGame.p1.pos = move(newGame.p1, steps)
-        inc newGame.p1.score, newGame.p1.pos
+        newGame.p1Pos = move(newGame.p1Pos, steps)
+        inc newGame.p1Score, newGame.p1Pos
       else:
-        newGame.p2.pos = move(newGame.p2, steps)
-        inc newGame.p2.score, newGame.p2.pos
+        newGame.p2Pos = move(newGame.p2Pos, steps)
+        inc newGame.p2Score, newGame.p2Pos
 
-      if newGame.won(newGame.p1):
+      if newGame.p1Score >= 21:
         inc result.p1Wins, paths * count
-      elif newGame.won(newGame.p2):
+      elif newGame.p2Score >= 21:
         inc result.p2Wins, paths * count
       else:
         let winCount = determineWinCount(newGame, paths * count)
         result.p1Wins = result.p1Wins + winCount.p1Wins
         result.p2Wins = result.p2Wins + winCount.p2Wins
 
-  let game = parseInputGame(winningScore = 21)
+  let game = parseInputGame()
   let winCount = determineWinCount(game, 1)
   result = if winCount.p1Wins > winCount.p2Wins: winCount.p1Wins else: winCount.p2Wins
 
 proc solveDay21*: Solution[int, int64] =
   result.part1 = part1()
-  result.part2 = part2()
+  # result.part2 = part2()
 
 when isMainModule:
   echo solveDay21()
