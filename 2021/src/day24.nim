e84cb93619244599c932b269b38158fd3300510f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-02 11:32:39 +0100

    Work

--- 2021/src/day24.nim
+++ 2021/src/day24.nim
@@ -1,27 +1,28 @@
-import helpers, std/[algorithm, deques, math, sequtils, strscans, strutils, strformat, tables]
+import helpers, std/[deques, math, sequtils, strutils]
 
-type Block = tuple[p1, p2, p3: int]
+type Instructions = tuple[p1, p2, p3: int]
 
-proc readInstructions: seq[Block] =
+proc readInstructions: seq[Instructions] =
   for instructionBlock in readInputStrings(day = 24).toSeq.distribute(14):
     let instructions = instructionBlock.mapIt(it.splitWhitespace)
     result.add (p1: instructions[4][^1].parseInt, p2: instructions[5][^1].parseInt, p3: instructions[15][^1].parseInt)
 
-func findMinMax(instructions: seq[Block]): array[14, (int, int)] =
-  var z = initDeque[int]()
-  for i, segment in instructions:
-    if segment.p1 == 1: z.addLast i else:
-      let
-        j = z.popLast
-        w = segment.p2 + instructions[j].p3
-        s = [max(-1 * w, 0), max(w, 0)]
-      for k, q in [i, j]:
-        result[q] = (9 - s[k], 1 + s[1 - k])
+func findMinMax(instructions: seq[Instructions]): array[14, tuple[min, max: int]] =
+  var z: Deque[int]
+  for i, instructionBlock in instructions:
+    if instructionBlock.p1 == 1:
+      z.addLast i
+    else:
+      let j = z.popLast
+      let w = instructionBlock.p2 + instructions[j].p3
+      let s = [max(-1 * w, 0), max(w, 0)]
+      result[i] = (9 - s[0], 1 + s[1])
+      result[j] = (9 - s[1], 1 + s[0])
 
 proc solveDay24*: Solution[int64, int64] =
   let minMax = readInstructions().findMinMax()
-  result.part1 = minMax.foldl(a * 10 + b[0], 0)
-  result.part2 = minMax.foldl(a * 10 + b[1], 0)
+  result.part1 = minMax.foldl(a * 10 + b.min, 0)
+  result.part2 = minMax.foldl(a * 10 + b.max, 0)
 
 when isMainModule:
   echo solveDay24()
--- 2021/tests/tday24.nim
+++ 2021/tests/tday24.nim
@@ -3,8 +3,8 @@ import day24, std/unittest
 suite "day 24":
   test "part 1":
     let solution = solveDay24()
-    check: 679329 == solution.part1
+    check: 59996912981939 == solution.part1
 
   test "part 2":
     let solution = solveDay24()
-    check: 433315766324816 == solution.part2
+    check: 17241911811915 == solution.part2
