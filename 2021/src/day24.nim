99cf027b0ccd92a7d18cd55fdb9ca893ef563876
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-02 11:26:33 +0100

    2021: refactor

--- 2021/src/day24.nim
+++ 2021/src/day24.nim
@@ -1,114 +1,27 @@
 import helpers, std/[algorithm, deques, math, sequtils, strscans, strutils, strformat, tables]
 
-type Instructions = tuple[param1, param2: int]
+type Block = tuple[p1, p2, p3: int]
 
-# 0:inp w
-# 1:mul x 0
-# 2:add x z
-# 3:mod x 26
-# 4:div z 26 (or div z 1)
-# 5:add x -14 (or add x ...) (if < 0 then div z 26 else div z 1)
-# 6:eql x w
-# 7:eql x 0
-# 8:mul y 0
-# 9:add y 25
-# 10:mul y x
-# 11:add y 1
-# 12:mul z y
-# 13:mul y 0
-# 14:add y w
-# 15:add y 10 (or add y ...)
-# 16:mul y x
-# 17:add z y
+proc readInstructions: seq[Block] =
+  for instructionBlock in readInputStrings(day = 24).toSeq.distribute(14):
+    let instructions = instructionBlock.mapIt(it.splitWhitespace)
+    result.add (p1: instructions[4][^1].parseInt, p2: instructions[5][^1].parseInt, p3: instructions[15][^1].parseInt)
 
-# w = NUM
-# x = 0
-# x = x + z
-# x = x mod 26
-# z = z div 1 (or z = z div 26)
-# x = x + PARAM1
-# x = x == w (1 if true, 0 if false)
-# x = x == 0 
-# y = 0
-# y = y + 25
-# y = y * x
-# y = y + 1
-# z = z * y
-# y = 0
-# y = y + w
-# y = y + PARAM2
-# y = y * x
-# z = z + y
-
-# w = NUM
-# x = z
-# x = x mod 26
-# z = z div 1 (or z = z div 26)
-# x = x + PARAM1
-# x = x == w (1 if true, 0 if false)
-# x = x == 0 
-# y = (25 * x) + 1
-# z = z * y
-# y = (w + PARAM2) * x
-# z = z + y
-
-# SIMPLIFIED
-# PATH 1: div 1
-# w = NUM
-# x = if z mod 26 + PARAM1 == w: 0 else: 1
-#
-# PATH 2: div 26
-# w = NUM
-# x = if z mod 26 + PARAM1 == w: 0 else: 1
-# z = z div 26
-
-# TWO PATHS: x = 0 and x = 1
-#
-# PATH 1: x = 0
-# NOTHING CHANGES
-#
-# PATH 2: x = 1
-# z = (z * 26) + w + PARAM2
-
-func eval(instructions: Instructions, w, z: int): int =
-  let x =  if (z mod 26) + instructions.param1 == w: 0 else: 1
-  var z = if instructions.param1 < 0: z div 26 else: z
-  if x == 1: z = (z * 26) + w + instructions.param2
-  z
-
-  # SIMPLIFIED
-# PATH 1: div 1
-# w = NUM
-# x = if z mod 26 + PARAM1 == w: 0 else: 1
-#
-# PATH 2: div 26
-# w = NUM
-# x = if z mod 26 + PARAM1 == w: 0 else: 1
-# z = z div 26
-
-# TWO PATHS: x = 0 and x = 1
-#
-# PATH 1: x = 0
-# NOTHING CHANGES
-#
-# PATH 2: x = 1
-# z = (z * 26) + w + PARAM2
-
-proc readInstructions: seq[Instructions] =
-  let lines = readInputStrings(day = 24).toSeq
-  for instructions in lines.distribute(lines.len div 18):
-    let param1 = parseInt(instructions[5].splitWhitespace[^1])
-    let param2 = parseInt(instructions[15].splitWhitespace[^1])
-    result.add (param1: param1, param2: param2)
-
+func findMinMax(instructions: seq[Block]): array[14, (int, int)] =
+  var z = initDeque[int]()
+  for i, segment in instructions:
+    if segment.p1 == 1: z.addLast i else:
+      let
+        j = z.popLast
+        w = segment.p2 + instructions[j].p3
+        s = [max(-1 * w, 0), max(w, 0)]
+      for k, q in [i, j]:
+        result[q] = (9 - s[k], 1 + s[1 - k])
 
 proc solveDay24*: Solution[int64, int64] =
-  let instructions = readInstructions()
-
-  for num in 11111111111111 .. 11111111111111:
-    let digits = ($num).toSeq.mapIt(parseInt($it))
-    echo digits
-  # const initialState = {"w": 0, "x": 0, "y": 0, "z": 0}.toTable
+  let minMax = readInstructions().findMinMax()
+  result.part1 = minMax.foldl(a * 10 + b[0], 0)
+  result.part2 = minMax.foldl(a * 10 + b[1], 0)
 
 when isMainModule:
   echo solveDay24()
