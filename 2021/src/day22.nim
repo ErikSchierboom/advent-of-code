3af584920e78ae3d2244885656fddd10d9a03a42
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-02 12:54:50 +0100

    Work

--- 2021/src/day22.nim
+++ 2021/src/day22.nim
@@ -1,30 +1,33 @@
-import helpers, std/[sets, strscans, strutils]
+import helpers, std/[sets, sequtils, strscans, strutils]
 
 type 
-  Point3D = tuple[x, y, z: int]
-  Cuboid = tuple[on: bool, x, y, z: tuple[min, max: int]]
+  Cube = tuple[x, y, z: int]
+  Cuboid = tuple[on: bool, min, max: Cube]
 
-iterator readCuboids: Cuboid =
+proc readCuboids: seq[Cuboid] =
   for (_, on, xMin, xMax, yMin, yMax, zMin, zMax) in readInputScans(day = 22, pattern = "$w x=$i..$i,y=$i..$i,z=$i..$i"):
-    yield (on: on == "on", x: (min: xMin, max: xMax), y: (min: yMin, max: yMax), z: (min: zMin, max: zMax))
+    result.add (on: on == "on", min: (x: xMin, y: yMin, z: zMin), max: (x: xMax, y: yMax, z: zMax))
+
+iterator cubes(cuboid: Cuboid): Cube =
+  for x in cuboid.min.x..cuboid.max.x:
+    for y in cuboid.min.y..cuboid.max.y:
+      for z in cuboid.min.z..cuboid.max.z:
+        yield (x: x, y: y, z: z)
 
 proc solveDay22*: IntSolution =
-  var cubes: HashSet[Point3D]
+  let cuboids = readCuboids()
+
+  var cubes = initHashSet[Cube]()
 
   for cuboid in readCuboids():
-    if cuboid.x.min >= -50 and cuboid.x.max <= 50 and
-       cuboid.y.min >= -50 and cuboid.y.max <= 50 and
-       cuboid.z.min >= -50 and cuboid.z.max <= 50:
-      for x in cuboid.x.min..cuboid.x.max:
-        for y in cuboid.y.min..cuboid.y.max:
-          for z in cuboid.z.min..cuboid.z.max:
-              let cube = (x: x, y: y, z: z)
-              if cuboid.on:
-                cubes.incl cube
-              else:
-                cubes.excl cube
+    for x in cuboid.min.x..cuboid.max.x:
+      for y in cuboid.min.y..cuboid.max.y:
+        for z in cuboid.min.z..cuboid.max.z:
+          echo ""
+          # if cuboid.on: cubes.incl cube else: cubes.excl cube
 
-  result.part1 = cubes.len
+  # result.part1 = initCubes.len
+  # result.part2 = cubes.len
 
 when isMainModule:
   echo solveDay22()
