5adf0c2e03ed15cdbefd62dc3271938a89805143
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-03 09:23:32 +0100

    2021/22: solve
    2021/22: solve

--- 2021/src/day22.nim
+++ 2021/src/day22.nim
@@ -1,30 +1,44 @@
-import helpers, std/[sets, strscans, strutils]
+import helpers, std/[options, strscans]
 
 type 
   Point3D = tuple[x, y, z: int]
-  Cuboid = tuple[on: bool, x, y, z: tuple[min, max: int]]
+  Cuboid = tuple[on: bool, min, max: Point3D]
 
-iterator readCuboids: Cuboid =
+func volume(cuboid: Cuboid): int {.inline.} =
+  (if cuboid.on: 1 else: -1) * (cuboid.max.x - cuboid.min.x + 1) * (cuboid.max.y - cuboid.min.y + 1) * (cuboid.max.z - cuboid.min.z + 1)
+
+func `*`(left, right: Cuboid): Option[Cuboid] {.inline.} =
+  let min = (x: max(left.min.x, right.min.x), y: max(left.min.y, right.min.y), z: max(left.min.z, right.min.z))
+  let max = (x: min(left.max.x, right.max.x), y: min(left.max.y, right.max.y), z: min(left.max.z, right.max.z))
+  if max.x >= min.x and max.y >= min.y and max.z >= min.z:
+    result = some (on: not left.on, min: min, max: max)
+
+func inInitRegion(cuboid: Cuboid): bool {.inline.} =
+  cuboid.min.x in -50..50 and cuboid.max.x in -50..50 and
+  cuboid.min.y in -50..50 and cuboid.max.y in -50..50 and 
+  cuboid.min.z in -50..50 and cuboid.max.z in -50..50
+
+proc readCuboids: seq[Cuboid] =
   for (_, on, xMin, xMax, yMin, yMax, zMin, zMax) in readInputScans(day = 22, pattern = "$w x=$i..$i,y=$i..$i,z=$i..$i"):
-    yield (on: on == "on", x: (min: xMin, max: xMax), y: (min: yMin, max: yMax), z: (min: zMin, max: zMax))
+    result.add (on: on == "on", min: (x: xMin, y: yMin, z: zMin), max: (x: xMax, y: yMax, z: zMax))
 
 proc solveDay22*: IntSolution =
-  var cubes: HashSet[Point3D]
+  let cuboids = readCuboids()
+  var processedCuboids: seq[Cuboid]
 
-  for cuboid in readCuboids():
-    if cuboid.x.min >= -50 and cuboid.x.max <= 50 and
-       cuboid.y.min >= -50 and cuboid.y.max <= 50 and
-       cuboid.z.min >= -50 and cuboid.z.max <= 50:
-      for x in cuboid.x.min..cuboid.x.max:
-        for y in cuboid.y.min..cuboid.y.max:
-          for z in cuboid.z.min..cuboid.z.max:
-              let cube = (x: x, y: y, z: z)
-              if cuboid.on:
-                cubes.incl cube
-              else:
-                cubes.excl cube
+  for cuboid in cuboids:
+    var addToProcessed: seq[Cuboid]
 
-  result.part1 = cubes.len
+    for processed in processedCuboids:
+      let intersection = processed * cuboid
+      if intersection.isSome: addToProcessed.add intersection.get
+
+    processedCuboids.add addToProcessed
+    if cuboid.on: processedCuboids.add cuboid
+
+  for cuboid in processedCuboids:
+    if cuboid.inInitRegion: inc result.part1, cuboid.volume
+    inc result.part2, cuboid.volume
 
 when isMainModule:
   echo solveDay22()
