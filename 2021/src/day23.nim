9306d3f6e76b4650d7a9ff779b6a2cfbd2cc152e
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-09 13:00:43 +0100

    Work

--- 2021/src/day23.nim
+++ 2021/src/day23.nim
@@ -60,9 +60,10 @@ proc moves(state: State): seq[State] =
           let bottomOfRoomPoint = (x: point.x, y: point.y + 1)
           if bottomOfRoomPoint in state.amphipods:
             if state.amphipods[bottomOfRoomPoint] == kind:
-              echo $point & "," & $kind & ": in correct room at top with same kind at bottom"
+              continue
+              # echo $point & "," & $kind & ": in correct room at top with same kind at bottom"
             else:
-              echo $point & "," & $kind & ": in correct room at top with different kind at bottom"
+              # echo $point & "," & $kind & ": in correct room at top with different kind at bottom"
 
               for x in hallwayX:
                 if x < point.x:
@@ -80,18 +81,20 @@ proc moves(state: State): seq[State] =
                 newState.amphipods.del(point)
                 newState.amphipods[(x: x, y: hallwayY)] = kind
                 result.add newState
+                assert(newState.amphipods.len == 8)
           else:
-            echo $point & "," & $kind & ": in correct room at top with bottom empty"
+            # echo $point & "," & $kind & ": in correct room at top with bottom empty"
             var newState = state
             inc newState.energy, kindCost[kind]
             newState.amphipods.del(point)
             newState.amphipods[(x: point.x, y: point.y + 1)] = kind
             result.add newState
-        else:
-          echo $point & "," & $kind & ": in correct room at bottom"
+            assert(newState.amphipods.len == 8)
+        # else:
+          # echo $point & "," & $kind & ": in correct room at bottom"
       else:
         if inTopOfRoom(point):
-          echo $point & "," & $kind & ": in wrong room at top"
+          # echo $point & "," & $kind & ": in wrong room at top"
 
           for x in hallwayX:
             if x < point.x:
@@ -109,12 +112,14 @@ proc moves(state: State): seq[State] =
             newState.amphipods.del(point)
             newState.amphipods[(x: x, y: hallwayY)] = kind
             result.add newState
+            assert(newState.amphipods.len == 8)
         else:
           let topOfRoomPoint = (x: point.x, y: point.y - 1)
           if topOfRoomPoint in state.amphipods:
-            echo $point & "," & $kind & ": in wrong room at bottom and top blocked"
+            continue
+            # echo $point & "," & $kind & ": in wrong room at bottom and top blocked"
           else:
-            echo $point & "," & $kind & ": in wrong room at bottom and no top"
+            # echo $point & "," & $kind & ": in wrong room at bottom and no top"
 
             for x in hallwayX:
                 if x < point.x:
@@ -132,22 +137,61 @@ proc moves(state: State): seq[State] =
                 newState.amphipods.del(point)
                 newState.amphipods[(x: x, y: hallwayY)] = kind
                 result.add newState
+                assert(newState.amphipods.len == 8)
     else:
       let room = kinds.find(kind)
       let topRoomPos = (x: roomsX[room], y: roomsY[0])
       let bottomRoomPos = (x: roomsX[room], y: roomsY[0])
 
       if bottomRoomPos notin state.amphipods and topRoomPos notin state.amphipods:
-        echo $point & "," & $kind & ": in hallway and correct room has both spots empty"
-      elif bottomRoomPos notin state.amphipods and topRoomPos in state.amphipods:
-        echo $point & "," & $kind & ": in hallway and correct room has top spot occupied"
-      elif bottomRoomPos in state.amphipods and topRoomPos notin state.amphipods:
-        if state.amphipods[bottomRoomPos] == kind:
-          echo $point & "," & $kind & ": in hallway and correct room has top spot empty and bottom spot of same kind"
+        # echo $point & "," & $kind & ": in hallway and correct room has both spots empty"
+
+        if bottomRoomPos.x < point.x:
+          for x1 in bottomRoomPos.x + 1 .. point.x:
+            if (x: x1, y: hallwayY) in state.amphipods:
+              continue
         else:
-          echo $point & "," & $kind & ": in hallway and correct room has top spot empty and bottom spot of different kind"
-      elif bottomRoomPos in state.amphipods and topRoomPos in state.amphipods:
-        echo $point & "," & $kind & ": in hallway and correct room has both spots occupied"
+          for x1 in point.x .. bottomRoomPos.x - 1:
+            if (x: x1, y: hallwayY) in state.amphipods:
+              continue
+
+        let moves = abs(point.x - bottomRoomPos.x) + 2
+        var newState = state
+        inc newState.energy, (kindCost[kind] * moves)
+        newState.amphipods.del(point)
+        newState.amphipods[(x: bottomRoomPos.x, y: bottomRoomPos.y)] = kind
+        result.add newState
+
+        assert(newState.amphipods.len == 8)
+
+      # elif bottomRoomPos notin state.amphipods and topRoomPos in state.amphipods:
+      #   echo $point & "," & $kind & ": in hallway and correct room has top spot occupied"
+      # el
+      if bottomRoomPos in state.amphipods and topRoomPos notin state.amphipods:
+        if state.amphipods[bottomRoomPos] == kind:
+          # echo $point & "," & $kind & ": in hallway and correct room has top spot empty and bottom spot of same kind"
+
+          if topRoomPos.x < point.x:
+            for x1 in topRoomPos.x + 1 .. point.x:
+              if (x: x1, y: hallwayY) in state.amphipods:
+                continue
+          else:
+            for x1 in point.x .. topRoomPos.x - 1:
+              if (x: x1, y: hallwayY) in state.amphipods:
+                continue
+
+          let moves = abs(point.x - topRoomPos.x) + 1
+          var newState = state
+          inc newState.energy, (kindCost[kind] * moves)
+          newState.amphipods.del(point)
+          newState.amphipods[(x: topRoomPos.x, y: topRoomPos.y)] = kind
+          result.add newState
+          assert(newState.amphipods.len == 8)
+
+        # else:
+        #   echo $point & "," & $kind & ": in hallway and correct room has top spot empty and bottom spot of different kind"
+      # elif bottomRoomPos in state.amphipods and topRoomPos in state.amphipods:
+      #   echo $point & "," & $kind & ": in hallway and correct room has both spots occupied"
 
 proc part1(state: State): int =
   var queue: HeapQueue[State]
@@ -155,25 +199,24 @@ proc part1(state: State): int =
 
   energyCounts[$state.amphipods] = 0  
   queue.push state
-
-  echo energyCounts
   
   while queue.len > 0:
     let current = queue.pop()
 
     if current.amphipods.organized:
-      echo "organized"
+      # echo "organized"
+      echo current.amphipods
       return current.energy
 
     if current.energy > energyCounts.getOrDefault($current.amphipods, high(int)):
-      echo "too much energey"
+      # echo "too much energey"
       continue
 
     for move in current.moves:
-      echo move
-      echo energyCounts.getOrDefault($move.amphipods, high(int))
+      # echo move
+      # echo energyCounts.getOrDefault($move.amphipods, high(int))
       if move.energy < energyCounts.getOrDefault($move.amphipods, high(int)):
-        echo "new energy"
+        # echo "new energy"
         queue.push move
         energyCounts[$move.amphipods] = move.energy
 
