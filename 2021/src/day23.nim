1f5f2fb98a5aa393e4eaf04f4107a4b6c5b6a073
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-25 17:37:27 +0100

    Work

--- 2021/src/day23.nim
+++ 2021/src/day23.nim
@@ -2,7 +2,7 @@ import helpers, std/[heapqueue, sequtils, tables]
 
 type 
   Grid = Table[Point, char]
-  State = tuple[grid: Grid, energy: int]
+  State = tuple[grid: Grid, energy: int, roomYs: seq[int]]
 
 const rooms = { 'A': 3, 'B': 5, 'C': 7, 'D': 9 }.toTable
 const costs = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }.toTable
@@ -24,24 +24,24 @@ func isHallwayClear(a, b: int, grid: Grid): bool =
 
   true
 
-func moves(a: char, p: Point, grid: Grid, rest: seq[int]): seq[Point] =
+func moves(a: char, p: Point, state: State): seq[Point] =
   if p.y == 1: # Hallway
-    if rest.anyIt(grid.getOrDefault((x: rooms[a], y: it), a) != a): return @[]
+    if state.roomYs.anyIt(state.grid.getOrDefault((x: rooms[a], y: it), a) != a): return @[]
     let
       x = if p.x < rooms[a]: p.x + 1 else: p.x - 1
-      y = rest.filterIt((x: rooms[a], y: it) notin grid).max
-    return @[(rooms[a], y)].filterIt(x.isHallwayClear(rooms[a], grid))
+      y = state.roomYs.toSeq.filterIt((x: rooms[a], y: it) notin state.grid).max
+    return @[(rooms[a], y)].filterIt(x.isHallwayClear(rooms[a], state.grid))
   # Room correct
-  if p.x == rooms[a] and (p.y .. rest.max).allIt(grid.getOrDefault((x: p.x, y: it)) == a):
+  if p.x == rooms[a] and (p.y .. state.roomYs.max).toSeq.allIt(state.grid.getOrDefault((x: p.x, y: it)) == a):
     return @[]
-  if (x: p.x, y: p.y - 1) in grid: # Top of room not empty
+  if (x: p.x, y: p.y - 1) in state.grid: # Top of room not empty
     @[]
   else:
-    [1, 2, 4, 6, 8, 10, 11].filterIt(p.x.isHallwayClear(it, grid)).mapIt((it, 1))
+    [1, 2, 4, 6, 8, 10, 11].filterIt(p.x.isHallwayClear(it, state.grid)).mapIt((it, 1))
 
-func moves(state: State, rest: seq[int]): seq[State] =
+func moves(state: State): seq[State] =
   for p, a in state.grid:
-    for next in moves(a, p, state.grid, rest):
+    for next in moves(a, p, state):
       var updated = state
       updated.grid.del((x: p.x, y: p.y))
       updated.grid[next] = a
@@ -50,7 +50,7 @@ func moves(state: State, rest: seq[int]): seq[State] =
 
 func `<`(a: State, b: State): bool = a.energy < b.energy
 
-func process(state: State, rest: seq[int]): int =
+func solve(state: State): int =
   var queue: HeapQueue[State]
   queue.push(state)
 
@@ -66,24 +66,26 @@ func process(state: State, rest: seq[int]): int =
     if current.energy > energyCounts.getOrDefault(current.grid, high(int)):
       continue
 
-    for move in current.moves(rest):
+    for move in current.moves:
       if move.energy < energyCounts.getOrDefault(move.grid, high(int)):
         queue.push move
         energyCounts[move.grid] = move.energy
 
 proc readInputState(lines: seq[string]): State =
-  for _, x in rooms:
-    for y in 2 ..< lines.high:
+  for y in 2 ..< lines.high:
+    for _, x in rooms:
       result.grid[(x: x, y: y)] = lines[y][x]
+  
+  result.roomYs = (2 ..< lines.high).toSeq
 
 proc part1*: int = 
   let lines = readInputStrings(day = 23).toSeq
-  readInputState(lines).process @[2, 3]
+  solve(readInputState(lines))
 
 proc part2*: int = 
   var lines = readInputStrings(day = 23).toSeq
   lines.insert(@["  #D#C#B#A#  ", "  #D#B#A#C#  "], pos = 3)
-  readInputState(lines).process @[2, 3, 4, 5]
+  solve(readInputState(lines))
 
 proc solveDay23*: IntSolution =
   result.part1 = part1()
