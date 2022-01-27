37ef8f90e4970d0fed9f74c1b1365fb9b563fd0b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-27 21:11:50 +0100

    Work

--- 2021/src/day23.nim
+++ 2021/src/day23.nim
@@ -1,7 +1,7 @@
 import helpers, std/[heapqueue, sequtils, strutils, tables]
 
 type 
-  Grid = tuple[roomSize: int, organized: string]
+  Grid = tuple[roomIdxs: array[4, seq[int]], hallwayIdxs: array[7, int]]
   State = tuple[cells: string, energy: int]
 
 const amphipods = "ABCD"
@@ -9,17 +9,13 @@ const costs = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }.toTable
 
 func manhattanDistance(a, b: Point): int = abs(a.x - b.x) + abs(a.y - b.y)
 func isHallwayClear(a, b: int, cells: string): bool = cells[min(a, b) .. max(a, b)].allIt(it == '0')
-func inHallway(state: State, grid: Grid, idx: int): bool = idx < 7
-func targetRoom(state: State, grid: Grid, amphipod: char): string =
-  let roomBeginIdx = 7 + amphipods.find(amphipod) * grid.roomSize
-  let roomEndIdx = roomBeginIdx + grid.roomSize - 1
-  state.cells[roomBeginIdx..roomEndIdx]
+func inHallway(state: State, grid: Grid, idx: int): bool = idx in grid.hallwayIdxs
 
-func inOrganizedRoom(state: State, grid: Grid, amphipod: char): bool =
-  targetRoom(state, grid, amphipod).allIt(it == amphipod)
+func inOrganizedRoom(state: State, grid: Grid, idx: int, amphipod: char): bool =
+  grid.roomIdxs[amphipods.find(amphipod)].allIt(state.cells[it] == amphipod)
 
-func inBlockedRoom(state: State, grid: Grid, idx: int): bool =
-  let roomBeginIdx = idx - 7 - (idx mod grid.roomSize)
+func inBlockedRoom(state: State, grid: Grid, idx: int, amphipod: char): bool =
+  grid.roomIdxs[amphipods.find(amphipod)].filterIt(it < idx).allIt(state.cells[it] == '.')
 
 proc moves(state: State, grid: Grid, oldIdx: int, amphipod: char): seq[int] =
   if state.inHallway(grid, oldIdx): 
@@ -30,9 +26,8 @@ proc moves(state: State, grid: Grid, oldIdx: int, amphipod: char): seq[int] =
 #       let x = if p.x < rooms[a]: p.x + 1 else: p.x - 1
 #       let y = state.roomYs.filterIt((x: rooms[a], y: it) notin state.cells).max
 #       return @[(rooms[a], y)].filterIt(x.isHallwayClear(rooms[a], state.cells))
-  elif state.inOrganizedRoom(grid, amphipod): 
-    return
-  elif state.inBlockedRoom(grid, oldIdx):
+  elif state.inOrganizedRoom(grid, oldIdx, amphipod) or
+       state.inBlockedRoom(grid, oldIdx, amphipod):
     return
   else:
     return
@@ -52,22 +47,17 @@ proc moves(state: State, grid: Grid): seq[State] =
 func `<`(a: State, b: State): bool = a.energy < b.energy
 
 func toGrid(state: State): Grid =
-  result.roomSize = (state.cells.len - 7) div 4
-
-  for _ in 0 ..< 7: # Hallway
-    result.organized.add '.'
+  result.roomSize = ( - 7) div 4
 
   for x in 0 ..< 4: # Rooms
     for y in 0 ..< result.roomSize:
       result.organized.add amphipods[x]
 
+  for x in 0 ..< 7: # Hallway
+    result.hallwayIdxs[x] = 
+
 proc solve(state: State): int =
   let grid = state.toGrid
-
-  echo targetRoom(state, grid, 'A')
-  echo targetRoom(state, grid, 'B')
-  echo targetRoom(state, grid, 'C')
-  echo targetRoom(state, grid, 'D')
 
   # TODO: flip rooms and hallway
 
@@ -80,7 +70,7 @@ proc solve(state: State): int =
   while queue.len > 0:
     let current = queue.pop()
 
-    if current.cells == grid.organized:
+    if current.cells == grid.isOrganized:
       return current.energy
 
     for move in current.moves(grid):
@@ -89,12 +79,12 @@ proc solve(state: State): int =
         costs[move.cells] = move.energy
 
 proc readInputState(lines: seq[string]): State =
-  for _ in 0 ..< 7: # Hallway
-    result.cells.add '.'
-
   for x in 0 ..< 4: # Rooms
     for y in 0 ..< lines.len - 3:
       result.cells.add lines[2 + y][3 + x * 2]
+
+  for _ in 0 ..< 7: # Hallway
+    result.cells.add '.'
 
 proc part1*: int = 
   let lines = readInputStrings(day = 23).toSeq
