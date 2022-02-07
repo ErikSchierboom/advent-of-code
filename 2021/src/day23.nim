fe30e37158a28f36ba6d8246fc978488e9889ecd
Erik Schierboom <erik_schierboom@hotmail.com>
2022-02-07 08:49:39 +0100

    Day 23 (#16)
    Day 23

--- 2021/src/day23.nim
+++ 2021/src/day23.nim
@@ -1,71 +1,59 @@
-import helpers, std/[heapqueue, sequtils, tables]
+import helpers, std/[heapqueue, math, sequtils, strutils, tables]
 
 type 
-  Grid = Table[Point, char]
-  State = tuple[grid: Grid, energy, roomSize: int]
+  Grid = tuple[hallway: array[7, int], rooms: array[4, seq[int]]]
+  State = tuple[grid: Grid, energy: int]
 
-const rooms = { 'A': 3, 'B': 5, 'C': 7, 'D': 9 }.toTable
-const costs = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }.toTable
-const hallwayXs = [1, 2, 4, 6, 8, 10, 11]
-const hallwayY = 1
+const rooms = [2, 4, 6, 8]
+const hallway = [0, 1, 3, 5, 7, 9, 10]
+const empty = -1
 
-func manhattanDistance(a, b: Point): int = abs(a.x - b.x) + abs(a.y - b.y)
+func cost(amphipod: int): int = 10 ^ amphipod
 
-func isOrganized(state: State): bool =
-  for kind, x in rooms:
-    for y in 0..<state.roomSize:
-      if state.grid.getOrDefault((x: x, y: 2 + y)) != kind:
-        return false
-  
-  true
+func hallwayClear(grid: Grid, a, b: int): bool =
+  hallway.filterIt(it in min(a, b)..max(a, b)).allIt(grid.hallway[hallway.find(it)] == empty)
 
-func isHallwayClear(a, b: int, grid: Grid): bool =
-  for x in min(a, b) .. max(a, b):
-    if (x: x, y: 1) in grid:
-      return false
+func move(state: State, amphipod, hallwayIdx, roomIdx, levelIdx: int): State = 
+  result = state
+  swap(result.grid.hallway[hallwayIdx], result.grid.rooms[roomIdx][levelIdx])
+  inc result.energy, amphipod.cost * (abs(hallway[hallwayIdx] - rooms[roomIdx]) + 1 + levelIdx)
 
-  true
+iterator movesFromHallway(state: State): State =
+  for hallwayIdx, amphipod in state.grid.hallway:
+    if amphipod == empty or
+       state.grid.rooms[amphipod].anyIt(it != empty and it != amphipod) or
+       not state.grid.hallwayClear(if hallway[hallwayIdx] < rooms[amphipod]: hallway[hallwayIdx] + 1 else: hallway[hallwayIdx] - 1, rooms[amphipod]):
+      continue
 
-func canMoveToRoom(a: char, p: Point, state: State): bool =
-  for y in 0..<state.roomSize:
-    if state.grid.getOrDefault((x: rooms[a], y: y + 2), a) != a:
-      return false
-  
-  true
+    let roomLevel = state.grid.rooms[amphipod].pairs.toSeq.filterIt(it.val == empty).mapIt(it.key).max
+    yield move(state, amphipod, hallwayIdx, amphipod, roomLevel)
 
-func canMoveToHallway(a: char, p: Point, state: State): bool =
-  for y in 0..<p.y:
-    if (x: p.x, y: y) in state.grid:
-      return false
+iterator movesFromRooms(state: State): State =
+  for roomIdx, room in state.grid.rooms:
+    if room.allIt(it == roomIdx) or room.allIt(it == empty):
+      continue
 
-  true
+    let roomLevel = room.pairs.toSeq.filterIt(it.val != empty).mapIt(it.key)[0]
+    for hallwayIdx in toSeq(hallway.low..hallway.high).filterIt(state.grid.hallwayClear(hallway[it], rooms[roomIdx])):
+      yield move(state, room[roomLevel], hallwayIdx, roomIdx, roomLevel)
 
-func inHallway(a: char, p: Point, state: State): bool = p.y == hallwayY
+iterator moves(state: State): State =
+  for move in state.movesFromHallway: yield move
+  for move in state.movesFromRooms: yield move
 
-func inOrganizedRoom(a: char, p: Point, state: State): bool =
-  p.x == rooms[a] and (p.y ..< state.roomSize + 2).toSeq.allIt(state.grid.getOrDefault((x: p.x, y: it)) == a)
+func toOrganized(state: State): Grid =
+  for i, _ in state.grid.hallway:
+    result.hallway[i] = -1
 
-func moves(a: char, p: Point, state: State): seq[Point] =
-  if inHallway(a, p, state):
-    if canMoveToRoom(a, p, state):
-      let x = if p.x < rooms[a]: p.x + 1 else: p.x - 1
-      let y = (2..<state.roomSize + 2).toSeq.filterIt((x: rooms[a], y: it) notin state.grid).max
-      result.add @[(rooms[a], y)].filterIt(x.isHallwayClear(rooms[a], state.grid))
-  elif not inOrganizedRoom(a, p, state) and canMoveToHallway(a, p, state):
-    result.add hallwayXs.filterIt(p.x.isHallwayClear(it, state.grid)).mapIt((it, 1))
-
-func moves(state: State): seq[State] =
-  for p, a in state.grid:
-    for next in moves(a, p, state):
-      var updated = state
-      updated.grid.del((x: p.x, y: p.y))
-      updated.grid[next] = a
-      inc updated.energy, costs[a] * manhattanDistance(p, next)
-      result.add updated
+  for j, room in state.grid.rooms:
+    for _ in room:
+      result.rooms[j].add j
 
 func `<`(a: State, b: State): bool = a.energy < b.energy
 
-func solve(state: State): int =
+proc solve(state: State): int =
+  let organized = state.toOrganized
+
   var queue: HeapQueue[State]
   queue.push(state)
 
@@ -75,7 +63,7 @@ func solve(state: State): int =
   while queue.len > 0:
     let current = queue.pop()
 
-    if current.isOrganized:
+    if current.grid == organized:
       return current.energy
 
     for move in current.moves:
@@ -84,11 +72,12 @@ func solve(state: State): int =
         energyCounts[move.grid] = move.energy
 
 proc readInputState(lines: seq[string]): State =
-  for y in 2 ..< lines.high:
-    for _, x in rooms:
-      result.grid[(x: x, y: y)] = lines[y][x]
-  
-  result.roomSize = lines.len - 3
+  for cell in result.grid.hallway.mitems:
+    cell = -1
+
+  for i, x in rooms:
+    for y in 2 ..< lines.high:
+      result.grid.rooms[i].add "ABCD".find(lines[y][x + 1])
 
 proc part1*: int = 
   let lines = readInputStrings(day = 23).toSeq
