ab26309c052baa4ce6d1e307db9b2f5a75688787
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-06 11:33:49 +0100

    2022/05: iterate

--- 2022/day05.rb
+++ 2022/day05.rb
@@ -1,15 +1,14 @@
 def parse_stack_and_moves
-  File.read('inputs/05.txt').then do |str|
-    str.split("\n\n").then do |top, bottom|
-      stacks = top.lines[0..-2]
-                  .map { |line| line.scan(/\[(.)\](?: |$)| ( ) (?: |$)/).map(&:first) }
-                  .transpose
-                  .map(&:compact)
-                  .map(&:reverse)
-      moves = bottom.scan(/^move (\d+) from (\d+) to (\d+)$/).map { |move| move.map(&:to_i) }
-      [stacks, moves]
-    end
-  end
+  File.readlines('inputs/05.txt', chomp: true)
+      .then { |lines| lines.slice_when { |line| line.empty? }.to_a }
+      .then do |(top, bottom)|
+        stacks = top[0..-4].map { |line| (1..33).step(4).map { |idx| line.chars[idx] unless line.chars[idx] == ' ' } }
+                           .transpose
+                           .map(&:compact)
+                           .map(&:reverse)
+        moves = bottom.map { |line| line.scan(/^move (\d+) from (\d+) to (\d+)$/).flatten.map(&:to_i) }
+        [stacks, moves]
+      end
 end
 
 def a_strategy(stacks, count, from, to) = stacks[to].push(*stacks[from].slice!(-count..).reverse)
