4848b1a93c74d00d67885ec1432009e0937384cf
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-15 10:10:41 +0100

    2022/14: iterate

--- 2022/day14.rb
+++ 2022/day14.rb
@@ -1,27 +1,34 @@
 require 'set'
 
-def parse_cave
-  File.readlines('inputs/14.txt', chomp: true).each_with_object(Set.new) do |path, cave|
+POUR_POINT = [500, 0].freeze
+
+def simulate(end_condition)
+  cave = File.readlines('inputs/14.txt', chomp: true).each_with_object(Set.new) do |path, cave|
     path.scan(/(\d+),(\d+)/).map { _1.map(&:to_i) }.each_cons(2).map(&:sort).each do |(from, to)|
       cave.merge(from[0].upto(to[0]).to_a.product(from[1].upto(to[1]).to_a))
     end
   end
-end
 
-POUR_POINT = [500, 0].freeze
-POUR_DIRECTIONS = [[0, 1], [-1, 1], [1, 1]].freeze
-
-def simulate(end_condition)
-  cave = parse_cave
   bottom = cave.map(&:last).max
   num_rocks = cave.size
 
-  while true
-    sand = POUR_POINT.dup
+  loop do
+    sand = POUR_POINT
 
-    while true
+    loop do
       break if sand[1] == bottom + 1
-      break unless POUR_DIRECTIONS.map {|(dx, dy)| [sand[0] + dx, sand[1] + dy] }.find { !cave.include?(_1) }&.tap { sand = _1 }
+
+      if !cave.include?([sand[0], sand[1] + 1])
+        sand[1] += 1
+      elsif !cave.include?([sand[0] - 1, sand[1] + 1])
+        sand[0] -= 1
+        sand[1] += 1
+      elsif !cave.include?([sand[0] + 1, sand[1] + 1])
+        sand[0] += 1
+        sand[1] += 1
+      else
+        break
+      end
     end
 
     cave << sand
@@ -29,8 +36,8 @@ def simulate(end_condition)
   end
 end
 
-a = simulate(-> (on_bottom, _on_top) { on_bottom }) - 1
-b = simulate(-> (_on_bottom, on_top) { on_top })
+a = simulate(->(on_bottom, _on_top) { on_bottom }) - 1
+b = simulate(->(_on_bottom, on_top) { on_top })
 
 require 'minitest/autorun'
 
