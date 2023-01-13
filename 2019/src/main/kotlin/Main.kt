ed38722343cfe32dacf4d070057096a86cea8e52
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-13 11:43:50 +0100

    2019: remove

--- 2019/src/main/kotlin/Main.kt
+++
@@ -1,4 +0,0 @@
-fun main() {
-    println("Day6/1: ${Day6.part1()}")
-    println("Day6/2: ${Day6.part2()}")
-}
--- 2019/src/main/kotlin/Resource.kt
+++
@@ -1,5 +0,0 @@
-fun readResourceAsText(path: String) = object {}.javaClass.getResource(path).readText()
-
-fun readResourceAsLines(path: String) = readResourceAsText(path).lines()
-
-fun readResourcesAsInts(path: String) = readResourceAsLines(path).map(String::toInt)
