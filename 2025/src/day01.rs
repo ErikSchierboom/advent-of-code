74391085c7143e5d1dbc55e6ef1559034e383db9
Erik Schierboom <erik_schierboom@hotmail.com>
2025-12-01 14:49:02 +0100

    2025/01: solve

--- 2025/src/day01.rs
+++ 2025/src/day01.rs
@@ -1,35 +1,60 @@
-use std::collections::HashSet;
+use std::ops::{Add, Sub};
 
-pub fn part_1(input: &str) -> i32 {
-    input
-        .lines()
-        .map(|number| number.parse::<i32>().unwrap())
-        .reduce(|accum, item| accum + item)
-        .unwrap()
+pub struct Rotation {
+    before: i32,
+    after: i32,
+    by: i32,
+}
+
+pub fn part_1(input: &str) -> usize {
+    rotations(input)
+        .filter(|rotation| rotation.after == 0)
+        .count()
 }
 
 pub fn part_2(input: &str) -> i32 {
-    let mut processed: HashSet<i32> = HashSet::new();
-    let mut accum = 0;
+    rotations(input)
+        .map(|rotation| {
+            let full_rotations = rotation.by.abs() / 100;
 
-    for number in input.lines().cycle() {
-        accum = accum + number.parse::<i32>().unwrap();
-        if !processed.insert(accum) {
-            return accum
-        }
-    }
+            if rotation.after == 0 {
+                full_rotations + 1
+            } else if rotation.by.signum() == rotation.before.sub(rotation.after).signum() * rotation.before.signum() {
+                full_rotations + 1
+            } else {
+                full_rotations
+            }
+        })
+        .sum()
+}
 
-    panic!("Can't reach")
+fn rotations(input: &str) -> impl Iterator<Item=Rotation> {
+    input
+        .lines()
+        .map(|rotation| if &rotation[0..1] == "L" { -1 } else { 1 } * rotation[1..].parse::<i32>().unwrap())
+        .scan(50, |current, rotated_by| {
+            let before = current.clone();
+            let after = current.add(rotated_by).rem_euclid(100);
+            *current = after;
+            Some(Rotation { before, after, by: rotated_by })
+        })
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::utils::read_input;
     use super::*;
 
-    const SAMPLE: &str = "+1\n\
-                          -2\n\
-                          +3\n\
-                          +1";
+    const SAMPLE: &str = "L68\n\
+                          L30\n\
+                          R48\n\
+                          L5\n\
+                          R60\n\
+                          L55\n\
+                          L1\n\
+                          L99\n\
+                          R14\n\
+                          L82";
 
     #[test]
     pub fn solve_part_1_example() {
@@ -37,7 +62,17 @@ mod tests {
     }
 
     #[test]
+    pub fn solve_part_1_input() {
+        assert_eq!(part_1(&read_input(1)), 1182);
+    }
+
+    #[test]
     pub fn solve_part_2_example() {
-        assert_eq!(part_2(SAMPLE), 2);
+        assert_eq!(part_2(SAMPLE), 6);
+    }
+
+    #[test]
+    pub fn solve_part_2_input() {
+        assert_eq!(part_2(&read_input(1)), 6907);
     }
 }
